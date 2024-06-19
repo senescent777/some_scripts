2dd06a789eeed05b5c184a2847438d3fff455b77
senescent777 <113385047+senescent777@users.noreply.github.com>
2024-06-19 10:22:56 +0000

    moving files to another dir, pt 1

---
+++ minimize/home/devuan/Desktop/minimize/pt2.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+sudo rm -rf /run/live/medium/live/initrd.img*
+sudo apt-get remove --purge --yes amd64-microcode atril* at-spi2-core coinor*
+sudo apt autoremove --yes
+
+sudo rm -rf /run/live/medium/live/initrd.img*
+sudo apt-get remove --purge --yes dirmngr discover* distro-info-data 
+sudo apt-get remove --purge --yes efibootmgr exfalso ftp gcr
+sudo apt autoremove --yes
+
+sudo rm -rf /run/live/medium/live/initrd.img*
+sudo apt-get remove --purge --yes ghostscript gir* gdisk gpg-*
+sudo apt-get remove --purge --yes gpg-agent gpgconf gpgsm gparted
+sudo apt autoremove --yes
+
+sudo rm -rf /run/live/medium/live/initrd.img*
+sudo apt-get remove --purge --yes gsasl* gstreamer* gsfonts* 
+sudo apt-get remove --purge --yes htop intel-microcode iucode-tool libgstreamer*
+sudo apt autoremove --yes
+
+sudo rm -rf /run/live/medium/live/initrd.img*
+sudo apt-get remove --purge --yes lm-sensors lvm2 mailcap mariadb-common
+sudo apt-get remove --purge --yes mokutil mysql-common openssh* orca
+sudo apt autoremove --yes
+
+sudo rm -rf /run/live/medium/live/initrd.img*
+sudo apt-get remove --purge --yes os-prober po* ppp proc*
+sudo apt-get remove --purge --yes ristretto screen shim* samba*
+sudo apt autoremove --yes
+
+sudo rm -rf /run/live/medium/live/initrd.img*
+sudo apt-get remove --purge --yes speech* system-config* telnet tex*
+sudo apt-get remove --purge --yes udisks2 uno* ure* upower
+sudo apt autoremove --yes
+
+sudo rm -rf /run/live/medium/live/initrd.img*
+sudo apt-get remove --purge --yes vim* xorriso
+sudo apt autoremove --yes
+sudo rm -rf /run/live/medium/live/initrd.img*
