ad3403aae9444fcc77c7f4def223f8d14eff6568
senescent777 <113385047+senescent777@users.noreply.github.com>
2024-06-21 15:48:25 +0000

    alt. version which does not yet work properly
    To be merged into main branch when it works.

--- minimize/home/devuan/Desktop/minimize/doIt6.sh
+++ minimize/home/devuan/Desktop/minimize/doIt6.sh
@@ -4,7 +4,7 @@ iface=eth0 #grep /e/n/i ?
 debug=0
 the_ar=0
 tblz4=rules.v4.180624
-install=0 #debug ja install komentoriviparam kautta jatkossa
+install=0 
 tgtfile=/mnt/186/nu.tar
 enforce=0
 no_mas=0
@@ -22,7 +22,6 @@ sip=$(sudo which ip)
 snt=$(sudo which netstat)
 sdi=$(sudo which dpkg)
 
-#DONE:optioiden parsintaan parannusta, nyt vain -o tarvitsee $2 oikeasti
 parse_opts_2() {
 	case "${1}" in
 		-o )
@@ -50,13 +49,9 @@ parse_opts_1() {
 
 if [ $# -gt 0 ] ; then
 	parse_opts_2 ${1} ${2}
-	#parse_opts ${3} ${4}
-	#parse_opts ${5} ${6}
-
 	for opt in $@ ; do parse_opts_1 $opt ; done
 fi
 
-#DONE:tables-juttujen toiminnan tarkistus tähän samaan
 check_params() {
 	case ${the_ar} in
 		0|1)
@@ -79,7 +74,7 @@ check_params() {
 	esac
 
 	[ -s ${tgtfile} ] && echo "${tgtfile} alr3ady 3x1st5"
-		
+	#TODO:tgtfile'n hakemisto olemassa?	
 	
 	case ${no_mas} in
 		0|1)
@@ -117,8 +112,20 @@ check_binaries() {
 	[ -x ${snt} ] || exit 5
 	[ -x ${sdi} ] || exit 5
 
-	[ ${debug} -eq 1 ] && echo "b1nar135 0k"
-	[ ${debug} -eq 1 ] && sleep 3
+	if [ ${debug} -eq 1 ] ; then
+		echo "b1nar135 0k" #
+		sleep 3 #
+	fi #
+
+	ipt="sudo ${ipt}"
+	ip6t="sudo ${ip6t}"
+	iptr="sudo ${iptr}"
+	ip6tr="sudo ${ip6tr}"
+	whack="sudo ${whack} --signal 9 "
+	snt="sudo ${snt}"
+	sharpy="sudo ${sag} remove --purge --yes "
+	sdi="sudo ${sdi} -i"
+	shary="sudo ${sag} --no-install-recommends reinstall "
 }
 
 check_params
@@ -142,19 +149,19 @@ if [ ${enforce} -eq 1 ] ; then
 else
 	if [ ${debug} -eq 1 ] ; then 
 		#VAIH:testaa tästä eteenpäin ch-kikkailut (tilap jemmassa)
-		echo "https://github.com/senescent777/project/tree/main/sbin can be used as /sbin"
-		echo "sudo chown -R root:root /sbin"
-		echo "sudo chmod -R 0755 /sbin"
+		echo "#https://github.com/senescent777/project/tree/main/sbin can be used as /sbin"
+		echo "#sudo chown -R root:root /sbin"
+		echo "#sudo chmod -R 0755 /sbin"
 	
-		echo "https://github.com/senescent777/project/tree/main/etc can be used as /etc except for stubby-related stuff"
-		echo "sudo chown -R root:root /etc"
-		echo "sudo chmod -R go-w /etc"
+		echo "#https://github.com/senescent777/project/tree/main/etc can be used as /etc except for stubby-related stuff"
+		echo "#sudo chown -R root:root /etc"
+		echo "#sudo chmod -R go-w /etc"
 	
-		echo "sudo chown -R root:root /var"
-		echo "sudo chmod -R go-w /var"
-		echo "sudo chown root:root /"
-		echo "sudo chmod 0755 /"
-	fi
+		echo "#sudo chown -R root:root /var"
+		echo "#sudo chmod -R go-w /var"
+		echo "#sudo chown root:root /"
+		echo "#sudo chmod 0755 /"
+	fi #
 fi
 
 if [ -s /etc/resolv.conf.new ] && [ -s /etc/resolv.conf.OLD ] ; then
@@ -169,16 +176,18 @@ sudo ip link set ${iface} down
 #exit
 
 for t in INPUT OUTPUT FORWARD ; do 
-	sudo ${ipt} -P ${t} DROP
-	sudo ${ip6t} -P ${t} DROP
-	sudo ${ip6t} -F ${t}
+	${ipt} -P ${t} DROP
+	${ip6t} -P ${t} DROP
+	${ip6t} -F ${t}
 done
 
-for t in INPUT OUTPUT FORWARD b c e f ; do sudo ${ipt} -F ${t} ; done
+for t in INPUT OUTPUT FORWARD b c e f ; do ${ipt} -F ${t} ; done
 
-[ ${debug} -eq 1 ] && sudo ${ipt} -L
-[ ${debug} -eq 1 ] && sudo ${ip6t} -L
-[ ${debug} -eq 1 ] && sleep 5
+if [ ${debug} -eq 1 ] ; then
+	${ipt} -L #
+	${ip6t} -L #
+	sleep 5 
+fi #
 
 #exit
 
@@ -189,25 +198,27 @@ done
 
 [ ${debug} -eq 1 ] && echo "shutting down some services in 3 secs"
 sleep 3 
-sudo /usr/bin/pkill --signal 9 cups*
-sudo /usr/bin/pkill --signal 9 avahi*
-sudo /usr/bin/pkill --signal 9 dnsmasq*
-sudo /usr/bin/pkill --signal 9 stubby*
+${whack} cups*
+${whack} avahi*
+${whack} dnsmasq*
+${whack} stubby*
+${whack} nm-applet
 sleep 3
 
-sudo apt-get remove --purge --yes libblu* network* libcupsfilters* libgphoto*
-sudo apt-get remove --purge --yes avahi* blu* cups* exim*
-sudo apt-get remove --purge --yes rpc* nfs* ntp* sntp*
-sudo apt-get remove --purge --yes modem* wireless* wpa* iw lm-sensors
-#sudo apt-get remove --purge --yes  #mdadm jälkimmäisen poisTo saattaa olla huono idea
+${sharpy} libblu* network* libcupsfilters* libgphoto*
+${sharpy} avahi* blu* cups* exim*
+${sharpy} rpc* nfs* ntp* sntp*
+${sharpy} modem* wireless* wpa* iw lm-sensors
+#  #mdadm jälkimmäisen poisTo saattaa olla huono idea
 
 sudo rm -rf /run/live/medium/live/initrd.img*
 sleep 3
 #exit
 
 if [ ${the_ar} -eq 1 ] ; then 
-	[ ${debug} -eq 1 ] && echo "autoremove in 5 secs"
-	[ ${debug} -eq 1 ] && sleep 5
+	if [ ${debug} -eq 1 ] ; then
+		echo "autoremove in 5 secs"; sleep 5 #
+	fi #
 	sudo apt autoremove --yes
 else
 	[ ${debug} -eq 1 ] && echo "autoremove postponed"
@@ -217,36 +228,41 @@ fi
 sudo rm -rf /run/live/medium/live/initrd.img*
 sleep 3
 
-[ ${debug} -eq 1 ] && sudo netstat -tulpan
-[ ${debug} -eq 1 ] && sleep 5
+if [ ${debug} -eq 1 ] ; then
+	${snt} -tulpan
+	sleep 5
+fi #
 #exit
 
 add_doT() {
-	sudo ${ipt} -A b -p tcp --sport 853 -s ${1} -j c
-	sudo ${ipt} -A e -p tcp --dport 853 -d ${1} -j f
+	${ipt} -A b -p tcp --sport 853 -s ${1} -j c
+	${ipt} -A e -p tcp --dport 853 -d ${1} -j f
 }
 
 add_snd() {
-	sudo ${ipt} -A b -p udp -m udp -s ${1} --sport 53 -j ACCEPT 
-	sudo ${ipt} -A e -p udp -m udp -d ${1} --dport 53 -j ACCEPT 
+	${ipt} -A b -p udp -m udp -s ${1} --sport 53 -j ACCEPT 
+	${ipt} -A e -p udp -m udp -d ${1} --dport 53 -j ACCEPT 
 }
 
 astral_sleep() {
-	sudo ${ip6tr} /etc/iptables/rules.v6
-	sudo ${iptr} /etc/iptables/${tblz4}
+	[ ${debug} -eq 1 ] && echo "astral_sleep()"
+	${ip6tr} /etc/iptables/rules.v6
+	${iptr} /etc/iptables/${tblz4}
 
 	local s
 	for s in $(grep -v '#' /home/stubby/.stubby.yml | grep address_data | cut -d ':' -f 2) ; do add_doT ${s} ; done
 	for s in $(grep -v '#' /etc/resolv.conf.OLD | grep names | grep -v 127. | awk '{print $2}') ; do  add_snd ${s} ; done
 
-	[ ${debug} -eq 1 ] && sudo ${ipt} -L
-	[ ${debug} -eq 1 ] && sudo ${ip6t} -L
-	[ ${debug} -eq 1 ] && sleep 5
-}
+	if [ ${debug} -eq 1 ] ; then
+		${ipt} -L  #
+		${ip6t} -L #
+		sleep 5
+	fi #
+}	
 
 #part4.sh ?
 clouds() {
-	echo "coluds(${1})"
+	[ ${debug} -eq 1 ] && echo "coluds(${1})"
 
 	sudo rm /etc/resolv.conf
 	sudo rm /etc/dhcp/dhclient.conf
@@ -276,11 +292,11 @@ clouds 0
 #Tässä kohtaa mielekästä ajaa tables-komentoja vain jos s.a.autoremove:a EI ajettu.
 if [ ${the_ar} -eq 1 ] ; then 
 	if [ ${debug} -eq 1 ] ; then 
-		echo "sudo ${ip6tr} /etc/iptables/rules.v6"
+		echo "${ip6tr} /etc/iptables/rules.v6"
 		#DONE:lennosta rules.v4 mutilointia, yhdestä sun toisesta projektista mallia
-		echo "sudo ${iptr} /etc/iptables/${tblz4}"
-		echo "sudo ${ipt} -L;sleep 5"
-		echo "sudo ${ip6t} -L;sleep 5"
+		echo "${iptr} /etc/iptables/${tblz4}"
+		echo "${ipt} -L;sleep 5"
+		echo "${ip6t} -L;sleep 5"
 	fi
 else
 	astral_sleep
@@ -292,20 +308,20 @@ fi
 if [ ${install} -eq 1 ] ; then 
 	if [ ${debug} -eq 1 ] ; then 
 		echo "sudo /sbin/ifup ${iface} | sudo /sbin/ifup -a" #if there is > 1 interfaces...
-		echo "sudo apt-get update"
-		echo "sudo apt-get --no-install-recommends reinstall libip4tc2 libip6tc2 libxtables12 netbase libmnl0 libnetfilter-conntrack3 libnfnetlink0 libnftnl11 iptables"
+		echo "${sag} update"
+		echo "${shary} libip4tc2 libip6tc2 libxtables12 netbase libmnl0 libnetfilter-conntrack3 libnfnetlink0 libnftnl11 iptables"
 		sudo rm -rf /run/live/medium/live/initrd.img*
 
-		echo "sudo apt-get --no-install-recommends reinstall init-system-helpers netfilter-persistent iptables-persistent"
+		echo "${shary} init-system-helpers netfilter-persistent iptables-persistent"
 		echo "sudo rm -rf /run/live/medium/live/initrd.img*"
 
 		#VAIH:ntps-juttui mukaan koska tar-nalkutus päiväyksistä
-		echo "sudo apt-get --no-install-recommends reinstall python3-ntp ntpsec-ntpdate"
+		echo "${shary} python3-ntp ntpsec-ntpdate"
 
-		echo "sudo apt-get --no-install-recommends reinstall dnsmasq-base runit-helper"
+		echo "${shary} dnsmasq-base runit-helper"
 		echo "sudo rm -rf /run/live/medium/live/initrd.img*"
 
-		echo "sudo apt-get --no-install-recommends reinstall libgetdns10 libbsd0 libidn2-0 libssl1.1 libunbound8 libyaml-0-2 stubby"
+		echo "${shary} libgetdns10 libbsd0 libidn2-0 libssl1.1 libunbound8 libyaml-0-2 stubby"
 		echo "sudo rm -rf /run/live/medium/live/initrd.img*"
 
 		#VAIH:muutama muukin juttu mukaan, mallia https://github.com/senescent777/project/blob/main/opt/bin/install.sh , https://github.com/senescent777/project/blob/main/opt/bin/install.sh , https://github.com/senescent777/project/blob/main/home/devuan/Dpckcer/buildr/source/scripts/part4.sh (jatka vielä)
@@ -322,24 +338,32 @@ if [ ${install} -eq 1 ] ; then
 		echo "sudo tar -rvpf ${tgtfile} ~/Desktop/minimize /etc/apt /etc/sysctl.conf /etc/network /etc/init.d/stubby"
 		echo "sudo tar -rvpf ${tgtfile} /var/cache/apt/archives/*.deb"
 		echo "sudo /sbin/ifdown ${iface} | sudo /sbin/ifdown -a"
-	fi
+	fi #
 else
-	[ ${debug} -eq 1 ] && echo "not fetching pkgs"
+	if [ ${debug} -eq 1 ] ; then
+		echo "not fetching pkgs"
+	fi #
 fi
 
 #exit
 #tässä kohta atai vähän myöh vaihd konftdstpt
 #clouds 1
 
-[ ${debug} -eq 1 ] && echo "sleep 5"
-[ ${debug} -eq 1 ] && echo "don't save tables rules"
+if [ ${debug} -eq 1 ] ; then
+	echo "don't save tables rules"
+	sleep 5
+fi #
 
-sudo dpkg -i /var/cache/apt/archives/dns-root-data*.deb ; sudo rm -rf /var/cache/apt/archives/dns-root-data*.deb
-sudo dpkg -i /var/cache/apt/archives/lib*.deb
-[ $? -eq  0 ] && sudo rm -rf /var/cache/apt/archives/lib*.deb
+pkgdir=/var/cache/apt/archives
+${sdi} ${pkgdir}/dns-root-data*.deb 
+[ $? -eq 0 ] && sudo rm -rf ${pkgdir}/dns-root-data*.deb
+
+${sdi} /var/cache/apt/archives/lib*.deb
+[ $? -eq  0 ] && sudo rm -rf ${pkgdir}/lib*.deb
+
 #HUOM. ei kannattane vastata myöntävästi tallennus-kysymykseen?
-sudo dpkg -i /var/cache/apt/archives/*.deb
-[ $? -eq 0 ] && sudo rm -rf /var/cache/apt/archives/*.deb
+${sdi} ${pkgdir}/*.deb
+[ $? -eq 0 ] && sudo rm -rf ${pkgdir}/*.deb
 [ ${debug} -eq 1 ] && sleep 2
 
 #exit
@@ -347,21 +371,35 @@ sudo dpkg -i /var/cache/apt/archives/*.deb
 #20.6,24 tähän asti vissiin ok
 [ ${no_mas} -eq 1 ] && exit 
 
-[ ${debug} -eq 1 ] && echo "starting 2 3nabl3 stubby in 5 secs"
-[ ${debug} -eq 1 ] && sleep 5
+if [ ${debug} -eq 1 ] ; then
+	echo "starting 2 3nabl3 stubby in 5 secs"
+	sleep 5
+fi #
+
 clouds 1 #tässä vai ennen dpkg ?
 
 #sudo /etc/init.d/netfilter-persistent restart #VARMEMPI TOISELLA TAVALLA PRKL
-sudo ${ip6tr} /etc/iptables/rules.v6
-sudo ${ipt} -D e 3; sudo ${ipt} -D e 2
-sudo ${ipt} -D b 3; sudo ${ipt} -D b 2
-sudo ${ipt} -D INPUT 5; sudo ${ipt} -D OUTPUT 6
+if [ ${debug} -eq 1 ] ; then
+	echo "before:
+	${ipt} -L
+	sleep 5
+fi #
 
-sudo /etc/init.d/dnsmasq restart
-[ ${debug} -eq 1 ] && sudo ${ipt} -L
-sleep 3
 #exit
 
+${ip6tr} /etc/iptables/rules.v6
+${ipt} -D e 3; ${ipt} -D e 2
+${ipt} -D b 3; ${ipt} -D b 2
+${ipt} -D INPUT 5; ${ipt} -D OUTPUT 6
+sudo /etc/init.d/dnsmasq restart
+
+if [ ${debug} -eq 1 ] ; then 
+	echo "after:"
+	${ipt} -L
+fi #
+
+sleep 5
+#exit #210624:kuseeko tämän jälkeen jotain vai ei?
 #kts. https://github.com/senescent777/some_scripts/blob/main/lib/d227D33.sh.export liittyen
 
 ns2() {
@@ -369,32 +407,45 @@ ns2() {
 	sudo /usr/sbin/userdel ${1}
 	sudo adduser --system ${1}
 	sudo chmod go-w /home
-	[ ${debug} -eq 1 ] && ls -las /home;sleep 7
+
+	if [ ${debug} -eq 1 ] ; then
+		ls -las /home;sleep 7
+	fi #
 }
 
-ns2 stubby
-
 ns4() {
-	[ ${debug} -eq 1 ] && echo "ns4()"
+	if [ ${debug} -eq 1 ] ; then 
+		echo "ns4()"
+	fi #
+
 	sudo chmod u+w /run
 	sudo touch /run/${1}.pid
 	sudo chmod 0600 /run/${1}.pid
 	sudo chown $1:65534 /run/${1}.pid
 	sudo chmod u-w /run
-	[ ${debug} -eq 1 ] && sleep 5
 
-	sudo /usr/bin/pkill --signal 9 ${1}*
-	[ ${debug} -eq 1 ] && sleep 5
+	if [ ${debug} -eq 1 ] ; then
+		sleep 5
+	fi
 
-	[ ${debug} -eq 1 ] && echo "starting ${1} in 5 secs"
-	[ ${debug} -eq 1 ] && sleep 5
+	${whack} ${1}*
+
+	if [ ${debug} -eq 1 ] ; then
+		sleep 5
+		echo "starting ${1} in 5 secs"
+		sleep 5
+	fi #
 
 	sudo -u ${1} ${1} -g
 	echo $?
-	[ ${debug} -eq 1 ] && sleep 5
+
+	if [ ${debug} -eq 1 ] ; then
+		sleep 5
+	fi
 }
 
 ns4 stubby
+ns2 stubby
 
 #HUOM.200624:vissiin toimii jo stubby 
 #https://raw.githubusercontent.com/senescent777/project/main/sbin/dhclient-script.new
