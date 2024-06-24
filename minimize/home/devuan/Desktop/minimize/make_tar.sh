53e62b032ac5a2b7b5c35143a2e6c9b77cf48d02
senescent777 <113385047+senescent777@users.noreply.github.com>
2024-06-24 19:03:56 +0000

    mostly about make_tar.sh

--- minimize/home/devuan/Desktop/minimize/make_tar.sh
+++ minimize/home/devuan/Desktop/minimize/make_tar.sh
@@ -6,36 +6,45 @@ tgtfile=/tmp/out.tar
 check_binaries #jos lib hoitaisi tän+seur...
 check_binaries2
 mode=0
+frist=0
 
 function parse_opts_1 () {
 	case ${1} in
 		-v|--v)
 			debug=1
 		;;
+		-h)
+			echo "$0 <mode> [-o ouftile] [-i infile] [-v]"
+		;;
 		*)
-			mode=${1}
+			if [ ${frist} -eq 0 ] ; then 
+				mode=${1}
+				frist=1
+			fi
 		;;
 	esac
 }
 
 function parse_opts_2() {
 	case "${1}" in
-		-o|--o)
+		-o|--o|-i|--i)
 			tgtfile=${2}
 		;;
 	esac
 }
 
 function check_params() {
-	[ -s ${tgtfile} ] && echo "${tgtfile} alr3ady 3x1st5"
+	dqb "check_params( ${1} )"
+	[ -s ${1} ] && echo "${1} alr3ady 3x1st5"
 	local d
 	
-	d=$(dirname ${tgtfile})
+	d=$(dirname ${1})
 	[ -d ${d} ] || echo "no such dir as ${d}"
 }
 
 function make_tar() {
-	dqb "#make_tar (demo)"
+	dqb "make_tar ( ${1} )"
+	csleep 1
 #	dqb "${sifu} ${iface}"
 #
 #	echo "${sip} link set ${iface} up"
@@ -63,22 +72,23 @@ function make_tar() {
 	csleep 5
 
 	#some kind of retrovirus
-	sudo tar -cvpf ${tgtfile} /var/cache/apt/archives/*.deb ~/Desktop/minimize /etc/iptables /etc/network/interfaces*
-	sudo tar -rvpf ${tgtfile} /etc/sudoers.d/user_shutdown /etc/sudoers.d/meshuggah /home/stubby
+	sudo tar -cvpf ${1} /var/cache/apt/archives/*.deb ~/Desktop/minimize /etc/iptables /etc/network/interfaces*
+	sudo tar -rvpf ${1} /etc/sudoers.d/user_shutdown /etc/sudoers.d/meshuggah /home/stubby
 	local f;for f in $(find /etc -type f -name 'stubby*') ; do tar -rvpf /tmp/out.tar $f ; done
 	for f in $(find /etc -type f -name 'dns*') ; do tar -rvpf /tmp/out.tar $f ; done
-	sudo tar -rvpf ${tgtfile} /etc/init.d/net*
-	sudo tar -rvpf ${tgtfile} /etc/rcS.d/S*net*
+	sudo tar -rvpf ${1} /etc/init.d/net*
+	sudo tar -rvpf ${1} /etc/rcS.d/S*net*
 
 #HUOM. on kai joitain komentoja joilla nuo K01-linkit voisi luoda
-#	sudo tar -rvpf ${tgtfile} /etc/rcS.d/{S14netfilter-persistent,S15networking}
-	sudo tar -rvpf ${tgtfile} /etc/rc2.d/{K01avahi-daemon,K01cups,K01cups-browsed,S03dnsmasq,S03stubby}
-	sudo tar -rvpf ${tgtfile} /etc/rc3.d/{K01avahi-daemon,K01cups,K01cups-browsed,S03dnsmasq,S03stubby}	
+#	sudo tar -rvpf ${1} /etc/rcS.d/{S14netfilter-persistent,S15networking}
+	sudo tar -rvpf ${1} /etc/rc2.d/{K01avahi-daemon,K01cups,K01cups-browsed,S03dnsmasq,S03stubby}
+	sudo tar -rvpf ${1} /etc/rc3.d/{K01avahi-daemon,K01cups,K01cups-browsed,S03dnsmasq,S03stubby}	
 	csleep 5
 }
 
 function make_tar2() {
-	echo "#add some stuff from ghub"
+	dqb "make_tar2 ( ${1} )"
+	csleep 1
 
 	local p
 	local q	
@@ -89,7 +99,7 @@ function make_tar2() {
 		${shary} git
 	fi
 
-	echo "#csleep 5"
+	csleep 5
 	tig=$(sudo which git)
 
 	p=$(pwd)
@@ -116,11 +126,11 @@ function make_tar2() {
 
 	${sco} -R root:root ./etc; ${scm} -R a-w ./etc
 	${sco} -R root:root ./sbin; ${scm} -R a-w ./sbin
-	sudo tar -rvpf ${tgtfile} ./etc ./sbin
+	sudo tar -rvpf ${1} ./etc ./sbin
 	cd ${p}
 	
-	sudo tar -tf  ${tgtfile} > MANIFEST
-	sudo tar -rvpf ${tgtfile} ${p}/MANIFEST
+	sudo tar -tf ${1} > MANIFEST
+	sudo tar -rvpf ${1} ${p}/MANIFEST
 	
 #	echo "${sifd} ${iface}	"
 #	echo "${sifd} -a"
@@ -131,28 +141,38 @@ function make_tar2() {
 }
 
 function make_upgrade() {
+	dqb "make_upgrade(${1} )"
+	dqb "${sag} upgrade -u"
+
 	${sag} upgrade -u
-	sudo tar -cvpf /tmp/upgrade.tar /var/cache/apt/archives 
+	sudo tar -cvpf ${1} /var/cache/apt/archives 
 }
 
 if [ $# -gt 0 ] ; then
-	#parse_opts_2 ${1} ${2}
+	parse_opts_2 ${2} ${3}
 	for opt in $@ ; do parse_opts_1 ${opt} ; done
-	${sag_u}
-	[ $? -eq 0 ] || echo "/o/b/clouds.sh <mode> | ${sifu} -a | ${sifu} ${iface}"
+
+	${sag_u} #tilap pois pelistä 24.6.24
+	[ $? -eq 0 ] || echo "/o/b/clouds.sh <mode> | ${sifu} -a | ${sifu} ${iface}";exit
 else
-	echo "$0 -h"
+	echo "$0 -h";exit
 fi
+
+check_params ${tgtfile}
 
 #main()
 case ${mode} in
 	0)
-		check_params
-		make_tar
-		make_tar2
+		make_tar ${tgtfile}
+		make_tar2 ${tgtfile}
 	;;
 	1)
-		make_upgrade
+		make_upgrade ${tgtfile}
+	;;
+	2)
+		cd /
+		sudo tar -xvpf ${tgtfile}
+		part3
 	;;
 	*)
 		echo "-h"
