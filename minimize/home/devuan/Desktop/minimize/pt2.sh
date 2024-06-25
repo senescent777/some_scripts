6425147a101df65a5d81d9da9a66e4e14b6eaa05
senescent777 <113385047+senescent777@users.noreply.github.com>
2024-06-25 21:40:30 +0000

    lin ibcluded to pt2

--- minimize/home/devuan/Desktop/minimize/pt2.sh
+++ minimize/home/devuan/Desktop/minimize/pt2.sh
@@ -1,111 +1,94 @@
-#!/bin/sh
-#lib jatqssa käyttöön?
+#!/bin/bash
+. ./lib
 
-sudo rm -rf /run/live/medium/live/initrd.img*
-sudo apt-get remove --purge --yes amd64-microcode atril* at-spi2-core coinor*
+#==============================================================
+#HUOM! PAKETIT procps, mtools JA mawk JÄTETTÄVÄ RAUHAAN!!!
+#==============================================================
+
+${smr} -rf /run/live/medium/live/initrd.img*
+${sharpy} amd64-microcode atril* at-spi2-core coinor*
 sudo apt autoremove --yes
 
-sudo rm -rf /run/live/medium/live/initrd.img*
+${smr} -rf /run/live/medium/live/initrd.img*
 
-sudo apt-get remove --purge --yes dirmngr discover* distro-info-data efibootmgr exfalso ftp gcr
+${sharpy} dirmngr discover* distro-info-data efibootmgr exfalso ftp gcr
 sudo apt autoremove --yes
 
-sudo rm -rf /run/live/medium/live/initrd.img*
-sudo apt-get remove --purge --yes ghostscript gir* gdisk gpg-* gpgconf gpgsm gparted
+${smr} -rf /run/live/medium/live/initrd.img*
+${sharpy} ghostscript gir* gdisk gpg-* gpgconf gpgsm gparted
 sudo apt autoremove --yes
 
-sudo rm -rf /run/live/medium/live/initrd.img*
-sudo apt-get remove --purge --yes gsasl* gsfonts* gstreamer*
+${smr} -rf /run/live/medium/live/initrd.img*
+${sharpy} gsasl* gsfonts* gstreamer*
 sudo apt autoremove --yes
 
-sudo rm -rf /run/live/medium/live/initrd.img*
-sudo apt-get remove --purge --yes htop intel-microcode
+${smr} -rf /run/live/medium/live/initrd.img*
+${sharpy} htop intel-microcode
 sudo apt autoremove --yes
-
 
 #HUOM.240624:mawk ja mtools uutena, takas pois jos qsee
-sudo rm -rf /run/live/medium/live/initrd.img*
-sudo apt-get remove --purge --yes lvm2 mdadm
+${smr} -rf /run/live/medium/live/initrd.img*
+${sharpy} lvm2 mdadm
 sudo apt autoremove --yes
 sleep 6
 
-#josko ohittaisi ," This should NOT be done unless you know exactly what you are doing!"
-#sudo rm -rf /run/live/medium/live/initrd.img*
-#sudo apt-get remove --purge --yes mawk
-#sudo apt autoremove --yes
-#sleep 6
-#
-#sudo rm -rf /run/live/medium/live/initrd.img*
-#sudo apt-get remove --purge --yes mtools
-#sudo apt autoremove --yes
-
-sudo rm -rf /run/live/medium/live/initrd.img*
-sudo apt-get remove --purge --yes mailcap mariadb-common
+${smr} -rf /run/live/medium/live/initrd.img*
+${sharpy} mailcap mariadb-common
 sudo apt autoremove --yes
-sudo rm -rf /run/live/medium/live/initrd.img*
+${smr} -rf /run/live/medium/live/initrd.img*
 
-sudo apt-get remove --purge --yes mokutil mysql-common orca openssh*
+${sharpy} mokutil mysql-common orca openssh*
 sudo apt autoremove --yes
-sudo rm -rf /run/live/medium/live/initrd.img*
+${smr} -rf /run/live/medium/live/initrd.img*
 
-
-sudo apt-get remove --purge --yes speech* system-config* telnet tex* udisks2 uno* ure* upower
+${sharpy} speech* system-config* telnet tex* udisks2 uno* ure* upower
 sudo apt autoremove --yes
 
-sudo rm -rf /run/live/medium/live/initrd.img*
-sudo apt-get remove --purge --yes vim* xorriso xfburn
+${smr} -rf /run/live/medium/live/initrd.img*
+${sharpy} vim* xorriso xfburn
 sudo apt autoremove --yes
 
-sudo apt-get remove --purge --yes iucode-tool libgstreamer* os-prober po*
+${sharpy} iucode-tool libgstreamer* os-prober po*
 sudo apt autoremove --yes
-
-#=================================================
-#sudo apt-get remove --purge --yes proc* liikaa? jos tyytyisi procmailiin
-#=================================================
 
 #240625 näytti siltä wettä ppp tau procmail ei poistunut, kts toistuuko
-sudo rm -rf /run/live/medium/live/initrd.img*
-sudo apt-get remove --purge --yes ppp 
+${smr} -rf /run/live/medium/live/initrd.img*
+${sharpy} ppp 
 sudo apt autoremove --yes
 sleep 1
 
-sudo rm -rf /run/live/medium/live/initrd.img*
-sudo apt-get remove --purge --yes ristretto
+${smr} -rf /run/live/medium/live/initrd.img*
+${sharpy} ristretto
 sudo apt autoremove --yes 
 sleep 1
 
-sudo apt-get remove --purge --yes screen shim* samba* 
+${sharpy} screen shim* samba* 
 sudo apt autoremove --yes
 sleep 1
 
-sudo rm -rf /run/live/medium/live/initrd.img*
-sudo apt-get remove --purge --yes procmail
+${smr} -rf /run/live/medium/live/initrd.img*
+${sharpy} procmail
 sudo apt autoremove --yes
 sleep 1
 
-
-sudo rm -rf /run/live/medium/live/initrd.img*
-sudo apt-get remove --purge --yes squashfs-tools
+${smr} -rf /run/live/medium/live/initrd.img*
+${sharpy} squashfs-tools
 sudo apt autoremove --yes
 sleep 6
 
 #uutena 250624, pois jos qsee
-sudo rm -rf /run/live/medium/live/initrd.img*
-sudo apt-get remove --purge --yes grub*
-
+${smr} -rf /run/live/medium/live/initrd.img*
+${sharpy} grub*
 sudo apt autoremove --yes
 sleep 6
 
 #uutena 250624, pois jos qsee
-sudo rm -rf /run/live/medium/live/initrd.img*
-
-sudo apt-get remove --purge --yes libgsm*
-remove --purge --yes grub*
-
+${smr} -rf /run/live/medium/live/initrd.img*
+${sharpy} libgsm*
 sudo apt autoremove --yes
 sleep 6
 
-sudo rm -rf /run/live/medium/live/initrd.img*
+${smr} -rf /run/live/medium/live/initrd.img*
 sudo shred -fu /var/cache/apt/archives/*.deb
 df
 #mimimize-hmiston siivous kanssa?
