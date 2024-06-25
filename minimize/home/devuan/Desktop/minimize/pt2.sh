453c98b7330bb206dfbb41ab777083cabec9a2b6
senescent777 <113385047+senescent777@users.noreply.github.com>
2024-06-25 14:12:04 +0000

    change of shell + other stuff

--- minimize/home/devuan/Desktop/minimize/pt2.sh
+++ minimize/home/devuan/Desktop/minimize/pt2.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
-#lib jatqssa käyttöön?
+#!/bin/bash
+#TODO:lib jatqssa käyttöön?
+#==============================================================
+#HUOM! PAKETIT procps, mtools JA mawk JÄTETTÄVÄ RAUHAAN!!!
+#==============================================================
 
 sudo rm -rf /run/live/medium/live/initrd.img*
 sudo apt-get remove --purge --yes amd64-microcode atril* at-spi2-core coinor*
@@ -22,22 +25,11 @@ sudo rm -rf /run/live/medium/live/initrd.img*
 sudo apt-get remove --purge --yes htop intel-microcode
 sudo apt autoremove --yes
 
-
 #HUOM.240624:mawk ja mtools uutena, takas pois jos qsee
 sudo rm -rf /run/live/medium/live/initrd.img*
 sudo apt-get remove --purge --yes lvm2 mdadm
 sudo apt autoremove --yes
 sleep 6
-
-#josko ohittaisi ," This should NOT be done unless you know exactly what you are doing!"
-#sudo rm -rf /run/live/medium/live/initrd.img*
-#sudo apt-get remove --purge --yes mawk
-#sudo apt autoremove --yes
-#sleep 6
-#
-#sudo rm -rf /run/live/medium/live/initrd.img*
-#sudo apt-get remove --purge --yes mtools
-#sudo apt autoremove --yes
 
 sudo rm -rf /run/live/medium/live/initrd.img*
 sudo apt-get remove --purge --yes mailcap mariadb-common
@@ -48,7 +40,6 @@ sudo apt-get remove --purge --yes mokutil mysql-common orca openssh*
 sudo apt autoremove --yes
 sudo rm -rf /run/live/medium/live/initrd.img*
 
-
 sudo apt-get remove --purge --yes speech* system-config* telnet tex* udisks2 uno* ure* upower
 sudo apt autoremove --yes
 
@@ -58,10 +49,6 @@ sudo apt autoremove --yes
 
 sudo apt-get remove --purge --yes iucode-tool libgstreamer* os-prober po*
 sudo apt autoremove --yes
-
-#=================================================
-#sudo apt-get remove --purge --yes proc* liikaa? jos tyytyisi procmailiin
-#=================================================
 
 #240625 näytti siltä wettä ppp tau procmail ei poistunut, kts toistuuko
 sudo rm -rf /run/live/medium/live/initrd.img*
@@ -83,7 +70,6 @@ sudo apt-get remove --purge --yes procmail
 sudo apt autoremove --yes
 sleep 1
 
-
 sudo rm -rf /run/live/medium/live/initrd.img*
 sudo apt-get remove --purge --yes squashfs-tools
 sudo apt autoremove --yes
@@ -92,16 +78,12 @@ sleep 6
 #uutena 250624, pois jos qsee
 sudo rm -rf /run/live/medium/live/initrd.img*
 sudo apt-get remove --purge --yes grub*
-
 sudo apt autoremove --yes
 sleep 6
 
 #uutena 250624, pois jos qsee
 sudo rm -rf /run/live/medium/live/initrd.img*
-
 sudo apt-get remove --purge --yes libgsm*
-remove --purge --yes grub*
-
 sudo apt autoremove --yes
 sleep 6
 
