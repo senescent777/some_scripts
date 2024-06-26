55dd3fce2119e8746e675a348b2f66bf537a7998
senescent777 <113385047+senescent777@users.noreply.github.com>
2024-06-26 11:01:23 +0000

    going firther with lib
    , remember to test if this still works

--- minimize/home/devuan/Desktop/minimize/pt2.sh
+++ minimize/home/devuan/Desktop/minimize/pt2.sh
@@ -7,85 +7,85 @@
 
 ${smr} -rf /run/live/medium/live/initrd.img*
 ${sharpy} amd64-microcode atril* at-spi2-core coinor*
-sudo apt autoremove --yes
+${sa} autoremove --yes
 
 ${smr} -rf /run/live/medium/live/initrd.img*
 
 ${sharpy} dirmngr discover* distro-info-data efibootmgr exfalso ftp gcr
-sudo apt autoremove --yes
+${sa} autoremove --yes
 
 ${smr} -rf /run/live/medium/live/initrd.img*
 ${sharpy} ghostscript gir* gdisk gpg-* gpgconf gpgsm gparted
-sudo apt autoremove --yes
+${sa} autoremove --yes
 
 ${smr} -rf /run/live/medium/live/initrd.img*
 ${sharpy} gsasl* gsfonts* gstreamer*
-sudo apt autoremove --yes
+${sa} autoremove --yes
 
 ${smr} -rf /run/live/medium/live/initrd.img*
 ${sharpy} htop intel-microcode
-sudo apt autoremove --yes
+${sa} autoremove --yes
 
 #HUOM.240624:mawk ja mtools uutena, takas pois jos qsee
 ${smr} -rf /run/live/medium/live/initrd.img*
 ${sharpy} lvm2 mdadm
-sudo apt autoremove --yes
+${sa} autoremove --yes
 sleep 6
 
 ${smr} -rf /run/live/medium/live/initrd.img*
 ${sharpy} mailcap mariadb-common
-sudo apt autoremove --yes
+${sa} autoremove --yes
 ${smr} -rf /run/live/medium/live/initrd.img*
 
 ${sharpy} mokutil mysql-common orca openssh*
-sudo apt autoremove --yes
+${sa} autoremove --yes
 ${smr} -rf /run/live/medium/live/initrd.img*
 
 ${sharpy} speech* system-config* telnet tex* udisks2 uno* ure* upower
-sudo apt autoremove --yes
+${sa} autoremove --yes
 
 ${smr} -rf /run/live/medium/live/initrd.img*
 ${sharpy} vim* xorriso xfburn
-sudo apt autoremove --yes
+${sa} autoremove --yes
 
 ${sharpy} iucode-tool libgstreamer* os-prober po*
-sudo apt autoremove --yes
+${sa} autoremove --yes
 
 #240625 näytti siltä wettä ppp tau procmail ei poistunut, kts toistuuko
 ${smr} -rf /run/live/medium/live/initrd.img*
 ${sharpy} ppp 
-sudo apt autoremove --yes
+${sa} autoremove --yes
 sleep 1
 
 ${smr} -rf /run/live/medium/live/initrd.img*
 ${sharpy} ristretto
-sudo apt autoremove --yes 
+${sa} autoremove --yes 
 sleep 1
 
 ${sharpy} screen shim* samba* 
-sudo apt autoremove --yes
+${sa} autoremove --yes
 sleep 1
 
 ${smr} -rf /run/live/medium/live/initrd.img*
 ${sharpy} procmail
-sudo apt autoremove --yes
+${sa} autoremove --yes
 sleep 1
 
 ${smr} -rf /run/live/medium/live/initrd.img*
 ${sharpy} squashfs-tools
-sudo apt autoremove --yes
+${sa} autoremove --yes
 sleep 6
 
 #uutena 250624, pois jos qsee
 ${smr} -rf /run/live/medium/live/initrd.img*
 ${sharpy} grub*
-sudo apt autoremove --yes
+${sa} autoremove --yes
 sleep 6
 
 #uutena 250624, pois jos qsee
 ${smr} -rf /run/live/medium/live/initrd.img*
 ${sharpy} libgsm*
-sudo apt autoremove --yes
+${sa} autoremove --yes
 sleep 6
 
 ${smr} -rf /run/live/medium/live/initrd.img*
