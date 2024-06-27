b4e338cc4cfd55862195035e6c3d616c38293cc1
senescent777 <113385047+senescent777@users.noreply.github.com>
2024-06-27 20:10:45 +0000

    taking advanrago of lib pt 2

---
+++ minimize/home/devuan/Desktop/minimize/export.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+. ./conf
+
+#TODO:tuplavarmistus että validi /e/n/i tulee mukaan
+function parse_opts_1() {
+	case "${1}" in
+		-v|--v)
+			debug=1
+		;;
+		*)
+			archive=${1}
+		;;
+	esac
+}
+
+. ./lib
+
+if [ $# -gt 0 ] ; then
+	for opt in $@ ; do parse_opts_1 ${opt} ; done
+fi
+
+[ ${debug} -eq 1 ] && echo "part= $part , dir=$dir, archive= $archive"
+#exit
+cd /
+
+${som} $part $dir -o rw
+[ $? -eq 0 ] || exit 1
+sleep 3
+
+[ -f ${dir}/${archive} ] && ${spc} ${dir}/${archive} ${dir}/${archive}.OLD
+#[ $? -eq 0 ] || exit 2 #mikähän tässä kusee?
+[ -f /tmp/${archive} ] && ${spc} /tmp/${archive} ${dir}
+
+sleep 3
+#jos -T kätevämpi?
+${srat} -uvpf ${dir}/${archive} /opt/bin ~/Desktop/minimize /etc/iptables /etc/network/interfaces* /etc/sudoers.d/meshuggah /etc/dhcp/dhclient* /etc/resolv.conf* /sbin/dhclient-*  
+
+csleep 3
+[ ${debug} -eq 1 ] && ls -las ${dir};sleep 5
+sleep 6
+
+${uom} ${part}
+echo $?
+${uom} ${dir}
+cd ~/Desktop/minimize
+mount | grep /dev
---
+++ minimize/home/devuan/Desktop/minimize/import.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+. ~/Desktop/minimize/conf
+
+function parse_opts_1() {
+	case "${1}" in
+		-v|--v)
+			debug=1
+		;;
+		*)
+			archive=${1}
+		;;
+	esac
+}
+
+. ./lib
+
+if [ $# -gt 0 ] ; then
+	for opt in $@ ; do parse_opts_1 ${opt} ; done
+fi
+
+#VAIH:oleelllisista hmistoista backup ennen purkua?
+${srat} -cvpf /OLD.tar /etc /sbin /opt/bin /home/stubby /home/devuan/Desktop
+
+cd /
+${som} ${part} ${dir} -o ro
+${srat} -xvpf ${dir}/${archive}
+${uom} ${part}
+cd ~/Desktop/minimize
+
+#HUOM. tämän olisi kuvakkeen kanssa tarkoitus mennä jatkossa filesystem.squashfs sisälle
+#TODO:make_tar:ista gpgtar-jutut
+#TODO:https://github.com/senescent777/some_scripts/tree/senescent777-alt-version/skripts/export ja https://github.com/senescent777/some_scripts/tree/senescent777-alt-version/lib/export soveötaen
