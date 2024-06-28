73fcbfb5cfcd4e559c3356b13baa942ed06ba666
senescent777 <113385047+senescent777@users.noreply.github.com>
2024-06-28 11:34:05 +0000

    attempting to do something about the things in comments

--- minimize/home/devuan/Desktop/minimize/import.sh
+++ minimize/home/devuan/Desktop/minimize/import.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 . ~/Desktop/minimize/conf
+install_pkgs=0
 
 function parse_opts_1() {
 	case "${1}" in
 		-v|--v)
 			debug=1
+		;;
+		-i|--i)
+			install_pkgs=1
 		;;
 		*)
 			archive=${1}
@@ -18,8 +22,9 @@ if [ $# -gt 0 ] ; then
 	for opt in $@ ; do parse_opts_1 ${opt} ; done
 fi
 
-#VAIH:oleelllisista hmistoista backup ennen purkua?
-${srat} -cvpf /OLD.tar /etc /sbin /opt/bin /home/stubby /home/devuan/Desktop
+if [ ! -s /OLD.tar ] ; then 
+	${srat} -cvpf /OLD.tar /etc /sbin /opt/bin /home/stubby /home/devuan/Desktop
+fi
 
 cd /
 ${som} ${part} ${dir} -o ro
@@ -34,18 +39,20 @@ ${som} ${part} ${dir} -o ro
 #fi
 
 #		cd /
-#		${srat} -xvpf ${tgtfile}
-#		#vöib mennä päällekkäin import.sh kanssa
-#		${sdi} /var/cache/apt/archives/perl-modules-5.32*.deb
-#		[ $? -eq 0 ] && ${smr} -rf ${pkgdir}/perl-modules-5.32*.deb
-#
-#		part3
 
 ${srat} -xvpf ${dir}/${archive}
+
+if [ ${install_pkgs} ] ; then 
+	${srat} -xvpf ${tgtfile}
+	#vöib mennä päällekkäin import.sh kanssa
+	${sdi} /var/cache/apt/archives/perl-modules-5.32*.deb
+	[ $? -eq 0 ] && ${smr} -rf ${pkgdir}/perl-modules-5.32*.deb
+
+	part3
+fi
+
 ${uom} ${part}
 cd ~/Desktop/minimize
 
 #HUOM. tämän olisi kuvakkeen kanssa tarkoitus mennä jatkossa filesystem.squashfs sisälle
-#VAIH:make_tar:ista gpgtar-jutut
-#TODO:https://github.com/senescent777/some_scripts/tree/senescent777-alt-version/skripts/export ja https://github.com/senescent777/some_scripts/tree/senescent777-alt-version/lib/export soveötaen
-#VAIH:jatkossa myös pakettien päivitys tänän skriptin kautta?
+#TODO:https://github.com/senescent777/some_scripts/tree/senescent777-alt-version/skripts/export ja https://github.com/senescent777/some_scripts/tree/senescent777-alt-version/lib/export soveLtaen
