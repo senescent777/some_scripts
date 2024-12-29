18d55e6072bc47e3c2b8ed09f5adb45828254e61
senescent777 <113385047+senescent777@users.noreply.github.com>
2024-12-29 17:17:20 +0000

    another update before moving to experimental branch

--- minimize/opt/bin/cloudsd.sh
+++ minimize/opt/bin/cloudsd.sh
@@ -3,70 +3,78 @@
 smr=$(sudo which rm)
 ipt=$(sudo which iptables)
 ip6t=$(sudo which ip6tables)
+iptr=$(sudo which iptables-restore)
+ip6tr=$(sudo which ip6tables-restore)
 slinky=$(sudo which ln)
 spc=$(sudo which cp)
 slinky="${slinky} -s "
 sco=$(sudo which chown)
 scm=$(sudo which chmod)
 
-#TODO:mietittävä uusiksi nämä konftdsto-jutut
-#${smr} /etc/resolv.conf
-#${smr} /etc/dhcp/dhclient.conf
-#${smr} /sbin/dhclient-script
+#VAIH:mietittävä uusiksi nämä konftdsto-jutut
+${smr} /etc/resolv.conf
+${smr} /etc/dhcp/dhclient.conf
+${smr} /sbin/dhclient-script
 
 #tässä oikea paikka tables-muutoksille vai ei?
 if [ y"${ipt}" == "y" ] ; then
 	echo "SHOULD 1NSTALL TABL35"
 else
+	${iptr} /etc/iptables/rules.v4
+	${ip6tr} /etc/iptables/rules.v6
+	sleep 3
+
 	${ipt} -F b
 	${ipt} -F e
+
+	#pitäisiköhän liittyä nöiden noihin dns/dot-juyyuihin?
 	${ipt} -D INPUT 5
-	${ipt} -D OUTPUT 6
+	${ipt} -D OUTPUT 5
 fi
 
 function tod_dda() { 
-	${ipt} -A b -p tcp --sport 853 -s ${1} -j c
-        ${ipt} -A e -p tcp --dport 853 -d ${1} -j f
+	echo "${ipt} -A b -p tcp --sport 853 -s ${1} -j c"
+        echo "${ipt} -A e -p tcp --dport 853 -d ${1} -j f"
 }
 
 function dda_snd() {
-	#${ipt} -A b -p udp -m udp -s ${1} --sport 53 -j ACCEPT 
-	#${ipt} -A e -p udp -m udp -d ${1} --dport 53 -j ACCEPT
+	${ipt} -A b -p udp -m udp -s ${1} --sport 53 -j ACCEPT 
+	${ipt} -A e -p udp -m udp -d ${1} --dport 53 -j ACCEPT
 
-	${ipt} -A INPUT -p udp -s ${1} -j ACCEPT 
-	${ipt} -A OUTPUT -p udp -d ${1} -j ACCEPT 
+#	${ipt} -A INPUT -p udp -s ${1} -m udp  --sport 53 -j ACCEPT 
+#	${ipt} -A OUTPUT -p udp -d ${1} -m udp --dport 53 -j ACCEPT 
 }
 
 case ${1} in 
 	0)
-#		${slinky} /etc/resolv.conf.OLD /etc/resolv.conf
-#		${slinky} /etc/dhcp/dhclient.conf.OLD /etc/dhcp/dhclient.conf
-#		${spc} /sbin/dhclient-script.OLD /sbin/dhclient-script
+		${slinky} /etc/resolv.conf.OLD /etc/resolv.conf
+		${slinky} /etc/dhcp/dhclient.conf.OLD /etc/dhcp/dhclient.conf
+		${spc} /sbin/dhclient-script.OLD /sbin/dhclient-script
 
 		if [ y"${ipt}" == "y" ] ; then
 			echo "SHOULD 1NSTALL TABL35"
 		else
-			#${ipt} -A INPUT -p udp -m udp --sport 53 -j b 
-			#${ipt} -A OUTPUT -p udp -m udp --dport 53 -j e
+			${ipt} -A INPUT -p udp -m udp --sport 53 -j b 
+			${ipt} -A OUTPUT -p udp -m udp --dport 53 -j e
 
 			for s in $(grep -v '#' /etc/resolv.conf | grep names | grep -v 127. | awk '{print $2}') ; do dda_snd ${s} ; done	
 		fi
 	;;
 	1)
-#		if [ -s /etc/resolv.conf.new ] ; then
-#			echo "r30lv.c0nf alr3ady 3x15t5"
-#		else
-#			sudo touch /etc/resolv.conf.new
-#			sudo chmod a+w /etc/resolv.conf.new
-#			sudo echo "nameserver 127.0.0.1" > /etc/resolv.conf.new
-#			sudo chmod 0444 /etc/resolv.conf.new
-#			sudo chown root:root /etc/resolv.conf.new
-#		fi
-#
-#		${slinky} /etc/resolv.conf.new /etc/resolv.conf
-#		${slinky} /etc/dhcp/dhclient.conf.new /etc/dhcp/dhclient.conf
-#		${spc} /sbin/dhclient-script.new /sbin/dhclient-script
-#
+		if [ -s /etc/resolv.conf.new ] ; then
+			echo "r30lv.c0nf alr3ady 3x15t5"
+		else
+			sudo touch /etc/resolv.conf.new
+			sudo chmod a+w /etc/resolv.conf.new
+			sudo echo "nameserver 127.0.0.1" > /etc/resolv.conf.new
+			sudo chmod 0444 /etc/resolv.conf.new
+			sudo chown root:root /etc/resolv.conf.new
+		fi
+
+		${slinky} /etc/resolv.conf.new /etc/resolv.conf
+		${slinky} /etc/dhcp/dhclient.conf.new /etc/dhcp/dhclient.conf
+		${spc} /sbin/dhclient-script.new /sbin/dhclient-script
+
 		if [ y"${ipt}" == "y" ] ; then
 			echo "SHOULD 1NSTALL TABL35"
 		else
@@ -100,7 +108,10 @@ ${scm} 0750 /etc/iptables
 sleep 2
 
 #if [ ${debug} -eq 1 ] ; then
+	echo "PISDEO PRO SATANA"
 	${ipt} -L  #
+	sleep 6
+	echo "666"
 	${ip6t} -L #
 	sleep 5
 #fi #
