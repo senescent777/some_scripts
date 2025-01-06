4ba1d2c9d0e500c967d301ef62146d851c464594
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-01-06 19:17:47 +0000

    tables-relate4d changes, mostly

---
+++ minimize/home/devuan/Desktop/libd.sh
@@ -0,0 +1,250 @@
+#=================================================PART 0=====================================
+#grep /e/n/i ?
+
+odio=$(which sudo)
+#TODO:tähän toinenkin tarkistus ennen -x
+[ -x ${odio} ] || exit 666
+
+function dqb() {
+	[ ${debug} -eq 1 ] && echo ${1}
+}
+
+function csleep() {
+	[ ${debug} -eq 1 ] && sleep ${1}
+}
+
+function check_binaries() {
+	dqb "ch3ck_b1nar135()"
+	dqb "sudo= ${odio} "
+	csleep 1
+
+	ipt=$(sudo which iptables)
+	ip6t=$(sudo which ip6tables)
+	iptr=$(sudo which iptables-restore)
+	ip6tr=$(sudo which ip6tables-restore)
+	sco=$(sudo which chown)
+	scm=$(sudo which chmod)
+	whack=$(sudo which pkill)
+	sag=$(sudo which apt-get)
+	sa=$(sudo which apt)
+	sip=$(sudo which ip)
+	snt=$(sudo which netstat)
+	sdi=$(sudo which dpkg)
+	sifu=$(sudo which ifup)
+	sifd=$(sudo which ifdown)
+	smr=$(sudo which rm)
+	slinky=$(sudo which ln)
+	spc=$(sudo which cp)
+	srat=$(sudo which tar)
+	som=$(sudo which mount)
+	uom=$(sudo which umount)
+
+	dqb "half_fdone"
+	csleep 1
+
+	if [ y"${ipt}" == "y" ] ; then
+		echo "SHOULD INSTALL IPTABLES"
+		echo "cd \$pkgdir"
+		echo "sudo dpkg -i netfilter-persistent*.deb"
+		echo "sudo dpkg -i libip*.deb"
+		echo "sudo dpkg -i iptables_*.deb"
+		echo "sudo dpkg -i iptables-*.deb"
+		exit 55
+	fi
+
+	[ -x ${ipt} ] || exit 5
+	#jospa sanoisi ipv6.disable=1 isolinuxille ni ei tarttisi tässä säätää
+	[ -x ${ip6t} ] || exit 5
+	[ -x ${iptr} ] || exit 5
+	[ -x ${ip6tr} ] || exit 5
+	[ -x ${sco} ] || exit 5
+	[ -x ${scm} ] || exit 5
+	[ -x ${whack} ] || exit 5
+	[ -x ${sag} ] || exit 5
+	[ -x ${sa} ] || exit 5
+	[ -x ${sip} ] || exit 5
+	[ -x ${snt} ] || exit 5
+	[ -x ${sdi} ] || exit 5
+	[ -x ${sifu} ] || exit 5
+	[ -x ${sifd} ] || exit 5
+
+	[ -x ${smr} ] || exit 5
+	[ -x ${slinky} ] || exit 5
+	[ -x ${spc} ] || exit 5
+	[ -x ${srat} ] || exit 5
+	[ -x ${som} ] || exit 5
+	[ -x ${uom} ] || exit 5
+
+	#TODO:tulisi speksata sudolle tarkemmin millä param on ok noita komentoja ajaa
+	CB_LIST1="${ipt} ${ip6t} ${iptr} ${ip6tr} ${sco} ${scm} ${whack} ${sag} ${sa} ${sip} ${snt} ${sdi} ${sifu} ${sifd} ${smr} ${slinky} ${srat} ${spc} ${som} ${uom}"
+
+	dqb "spc= ${spc}"
+	dqb "b1nar135 0k" 
+	csleep 3
+}
+
+function check_binaries2() {
+	dqb "ch3ck_b1nar135.2()"
+
+	ipt="${odio} ${ipt} "
+	ip6t="${odio} ${ip6t} "
+	iptr="${odio} ${iptr} "
+	ip6tr="${odio} ${ip6tr} "
+
+	whack="${odio} ${whack} --signal 9 "
+	snt="${odio} ${snt} "
+	sharpy="${odio} ${sag} remove --purge --yes "
+	sdi="${odio} ${sdi} -i "
+
+	#HUOM. ${sag} VIIMEISENÄ
+	shary="${odio} ${sag} --no-install-recommends reinstall --yes "
+	sag_u="${odio} ${sag} update "
+	sag="${odio} ${sag} "
+
+	sco="${odio} ${sco} "
+	scm="${odio} ${scm} "
+	sip="${odio} ${sip} "
+	sa="${odio} ${sa} "
+	sifu="${odio} ${sifu} "
+	sifd="${odio} ${sifd} "
+
+	smr="${odio} ${smr} "
+	lftr="${smr} -rf /run/live/medium/live/initrd.img* "
+
+	slinky="${odio} ${slinky} -s "
+	spc="${odio} ${spc} "
+	srat="${odio} ${srat} "
+	asy="${odio} ${sa} autoremove --yes"
+	fib="${odio} ${sa} --fix-broken install"
+	som="${odio} ${som} "
+	uom="${odio} ${uom} "	
+
+	dqb "b1nar135.2 0k.2" 
+	csleep 3
+}
+
+function mangle2() {
+	if [ -f ${1} ] ; then 
+		dqb "MANGLED ${1}"
+		sleep 1
+		${scm} o-rwx ${1}
+		${sco} root:root ${1}
+		csleep 1
+	fi
+}
+
+##HUOM.220624:stubbyn asentumisen ja käynnistymisen kannalta sleep saattaa olla tarpeen
+##VAIH:stubbyn asennus toimiimaan taas (261224)
+#function ns2() {
+#	dqb "ns2( ${1} )"
+#
+#	${scm} u+w /home
+#
+#	${odio} /usr/sbin/userdel ${1}
+#	sleep 3
+#
+#	${odio} adduser --system ${1}
+#	sleep 1
+#	${scm} go-w /home
+#
+#	[ ${debug} -eq 1 ]  && ls -las /home
+#	sleep 7
+#}
+#
+#function ns4() {
+#	dqb "ns4( ${1} )"
+#
+#	${scm} u+w /run
+#	${odio} touch /run/${1}.pid
+#	${scm} 0600 /run/${1}.pid
+#	${sco} ${1}:65534 /run/${1}.pid
+#	${scm} u-w /run
+#
+#	sleep 5
+#	#TODO:pitäisi kai tarkistaa parametrin validius ennenq...
+#	${whack} ${1}*
+#	sleep 5
+#
+#	dqb "starting ${1} in 5 secs"
+#
+#	sleep 5
+#	${odio} -u ${1} ${1} -g
+#	echo $?
+#	sleep 1
+#	pgrep stubby*
+#	sleep 5
+#}
+#
+#=========================PART 0 ENDS HERE=================================================================
+
+function part1() {
+	#jos jokin näistä kolmesta hoitaisi homman...
+	${sifd} ${iface}
+	${sifd} -a
+	${sip} link set ${iface} down
+
+	[ $? -eq 0 ] || echo "PROBLEMS WITH NETWORK CONNECTION"
+	[ ${debug} -eq 1 ] && /sbin/ifconfig;sleep 5 
+
+	if [ y"${ipt}" == "y" ] ; then
+		for t in INPUT OUTPUT FORWARD ; do 
+			${ipt} -P ${t} DROP
+			${ip6t} -P ${t} DROP
+			${ip6t} -F ${t}
+		done
+
+		for t in INPUT OUTPUT FORWARD b c e f ; do ${ipt} -F ${t} ; done
+
+		if [ ${debug} -eq 1 ] ; then
+			${ipt} -L #
+			${ip6t} -L #
+			sleep 5 
+		fi #
+	else
+		echo "5H0ULD-1N\$TALL-1PTABL35!!!"
+	fi
+}
+
+function part3() {
+	${sdi} /var/cache/apt/archives/lib*.deb
+
+	if [ $? -eq  0 ] ; then
+		#nköjään ei riittävästitehty dnsmasq kannalta
+		dqb "part3.1 ok"
+		sleep 5
+		${smr} -rf ${pkgdir}/lib*.deb
+	else
+	 	dqb "exit 66"
+	fi
+
+	#ei kannattane vastata myöntävästi tallennus-kysymykseen?
+	${sdi} ${pkgdir}/*.deb
+	
+	if [ $? -eq  0 ] ; then
+		dqb "part3.2 ok"
+		sleep 5
+		${smr} -rf ${pkgdir}/lib*.deb
+	else
+	 	dqb "exit 67"
+	fi
+
+	csleep 2
+}
+
+check_binaries
+check_binaries2
+
+function gpo() {
+	local prevopt
+	local opt
+	prevopt=""
+
+	for opt in $@ ; do 
+		parse_opts_1 ${opt}
+		parse_opts_2 ${prevopt} ${opt}
+		prevopt=opt
+	done
+}
+
+#TODO:gpo jo käyttöön?
+#check_params ?
