917bdfa983f2ac67bdc5a354822cb860f06bdab7
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-01-12 19:35:09 +0000

    the automagick ddisease

--- minimize/home/devuan/Desktop/minimize/daedalus/update.sh
+++ minimize/home/devuan/Desktop/minimize/daedalus/update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-. ~/Desktop/minimize/daedalus/conf #jos jotenkin automaagisemmin jatqssa(TODO)
-. ~/Desktop/minimize/daedalus/lib.sh
+
+d=$(dirname $0)
+. ${d}/conf
+. ${d}/lib.sh
+
 ${scm} a-wx ~/Desktop/minimize/*.sh
 
 [ x"${1}" == "x" ] && exit
