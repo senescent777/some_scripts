b637fbb2d0f4d3577827d90e82601a93c42e701f
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-01-15 22:22:23 +0000

    changes for the sake of changes

--- minimize/home/devuan/Desktop/minimize/daedalus/update.sh
+++ minimize/home/devuan/Desktop/minimize/daedalus/update.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
-
 d=$(dirname $0)
-[ -s ${d}/conf ] && . ${d}/conf
-[ -s ${d}/lib.sh ] && . ${d}/lib.sh
-${scm} a-wx ~/Desktop/minimize/*.sh
+debug=1
 
+#TODO:tämä paska toimimaan 
+
+if [ -s ${d}/conf ] && [ -s ${d}/lib.sh ] ; then
+	. ${d}/conf
+	. ${d}/lib.sh
+
+	echo "${scm} a-wx ~/Desktop/minimize/*.sh"
+else
+	echo "TODO:fallback"
+fi
+
+debug=1
 [ x"${1}" == "x" ] && exit
+
+dqb "BEFORE"
 ${som} ${part} ${dir}
 csleep 3
-${som} | grep /dev
-[ -s ${1} ] && ${srat} -rpf ${1} ~/Desktop/*.desktop ~/Desktop/minimize
+${som} | grep ${part} 
+csleep 3
+
+e=$(dirname ${1})
+f=$(basename ${1})
+find ${e} -name ${f}
+dqb "find ${e} -name ${f}"
+csleep 3
+
+if [ -s ${1} ] ; then
+	${srat} -rpf ${1} ~/Desktop/*.desktop ~/Desktop/minimize
+else
+	echo "NO SUCH THING AS ${1} "
+fi
+
+dqb "AFTER"
 csleep 3
 ${uom} ${part} 
 csleep 3
-${som} | grep /dev
+${som} | grep ${part}
