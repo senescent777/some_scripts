48c94b528a73e134b9f0b8c65af1f7c97e5b4570
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-01-27 21:39:26 +0000

    enforce_access() seems to work now

--- minimize/home/devuan/Desktop/minimize/daedalus/doIt6.sh
+++ minimize/home/devuan/Desktop/minimize/daedalus/doIt6.sh
@@ -9,11 +9,9 @@ else
 	exit 111	
 fi
 
-
 #HUOM. 260125: suattaapi olla niinnii jotta "80 megaa vs 180" ei liity suoraan paketteihin
 #DONE?:/v/c/man-nalkutus (sopisi olla ainakin)
 #180125:/tmp-jekku kai toimii jo
-
 
 function parse_opts_1() {
 	case "${1}" in
@@ -26,10 +24,8 @@ function parse_opts_1() {
 	esac
 }
 
-
 #HUOM. mode otetaan jo parametriksi p_o_1:sessä, josko enforce kanssa?
  
-
 function check_params() {
 	case ${debug} in
 		0|1)
@@ -43,7 +39,6 @@ function check_params() {
 }
 
 #HUOM. _s - kutsun oltava ennenq check_binaries2() kutsutaan tjsp.
-
 #HUOM.2. ei niitä {sco}-juttuja ao. fktioon, varm vuoksi
 
 function mangle_s() {
@@ -52,47 +47,37 @@ function mangle_s() {
 	tgt=${2}
 	dqb "fr0m mangle_s(${1}, ${2}) : params_OK"; sleep 3
 
-
 	if [ -s ${1} ] ; then 
 		#chattr -ui ${1} #chattr ei välttämättä toimi overlay'n tai squashfs'n kanssa
 		#csleep 1
 		
-
 		sudo chmod 0555 ${1} #HUOM. miksi juuri 5? no six six six että suoritettavaan tdstoon ei tartte kirjoittaa
-
 		sudo chown root:root ${1} 
 		#chattr +ui ${1}
 
 		#csleep 1
-
 		local s
 		local n
 
 		n=$(whoami) #olisi myös %users...
 		s=$(sha256sum ${1})
 		sudo echo "${n} localhost=NOPASSWD: sha256: ${s} " >> ${tgt}
-
 		#sleep 1
-
 	else
 		dqb "no sucg file as ${1} "
 	fi
 }
-
-#170125:./daedalus/doIt6.sh: line 70: /etc/sudoers.d/meshuggah: Permission denied
 
 function pre_enforce() {
 	#HUOM.230624 /sbin/dhclient* joutuisi hoitamaan toisella tavalla q mangle_s	
 	local q
 	q=$(mktemp -d)	
 
-
 	#jotain tolkkua tähän if-blokkiin olisi hyvä saada
 	#if [ -f /etc/sudoers.d/meshuggah ] ; then
 	#	#sudo mv /etc/sudoers.d/meshuggah /etc/sudoers.d/meshuggah.0LD
 	#	[ $? -eq 0 ] && dqb "a51a kun05a"
 	#else	
-
 		dqb "sudo touch ${q}/meshuggah in 5 secs"
 		csleep 5
 		sudo touch ${q}/meshuggah
@@ -103,23 +88,18 @@ function pre_enforce() {
 		dqb "ANNOYING AMOUNT OF DEBUG"
 
 		sudo chown 1000:1000 ${q}/meshuggah
-
 		sudo chmod 0660 ${q}/meshuggah	
-
 
 		local f 
 		for f in ${CB_LIST1} ; do mangle_s ${f} ${q}/meshuggah ; done
 	
-
 		#TODO:clouds: a) nimeäminen fiksummin 
 		for f in /etc/init.d/stubby ~/Desktop/minimize/${distro}/clouds.sh /sbin/halt /sbin/reboot ; do mangle_s ${f} ${q}/meshuggah ; done
 	#fi
-
 	
 	if [ -s ${q}/meshuggah ] ; then
 		dqb "sudo mv ${q}/meshuggah /etc/sudoers.d in 5 secs"
 		csleep 5
-
 
 		sudo chmod a-wx ${q}/meshuggah
 		sudo chown root:root ${q}/meshuggah	
@@ -127,14 +107,16 @@ function pre_enforce() {
 	fi
 
 	#HUOM.190125 nykyään tapahtuu ulosheitto xfce:stä jotta sudo-muutokset tulisivat voimaan?
-
 	
 	sudo chmod 0440 /etc/sudoers.d/* #hmiston kuiteskin parempi olla 0750
 	sudo chmod 0750 /etc/sudoers.d 
 	sudo chown -R root:root /etc/sudoers.d
 }
 
-#HUOM.260125: uskaltaisikohanjokupäivä kokeilla taas että enforce=1 
+#HUOM.270125.1: pelkkä enforce=1 viimeisimmän mergen mukaisissa skripteissä -> login ok
+#HUOM.270125.2:entä alun chmod+chown? edelleen login ok
+#HUOM.270125.3: /etc sorkkiminen? vissiin ok
+#HUOM.270125.4 /var? ok edelleen
 function enforce_access() {
 	dqb "3nf0rc3_acc355()"
 
@@ -142,7 +124,7 @@ function enforce_access() {
 	${sco} root:root /home
 	${scm} 0755 /home
 
-	#HUOM.260125: swaattopipa loginin pykiminen aihtutua alustamatt0omasta mjasta n (tai sit ei)
+	#HUOM.260125: saattoipa loginin pykiminen aiheutua alustamatt0omasta mjasta n (tai sit ei)
 	local n
 	n=$(whoami)
 	dqb "${sco} -R ${n}:${n} ~"
@@ -150,17 +132,12 @@ function enforce_access() {
 	csleep 5
 	
 	local f
-	${scm} -R 0755 ~/Desktop/minimize #VAIH:voisi kai vähän jyrkentää, kts import tai export
-	#${scm} 0755 ~/Desktop/minimize	
-	#for f in $(find ~/Desktop/minimize -type d) ; do ${scm} 0755 ${f} ; done	
-	#for f in $(find ~/Desktop/minimize -type f) ; do ${scm} 0444 ${f} ; done	
-	#${scm} a+x ~/Desktop/minimize/${distro}/*.sh
+	${scm} 0755 ~/Desktop/minimize	
+	for f in $(find ~/Desktop/minimize -type d) ; do ${scm} 0755 ${f} ; done	
+	for f in $(find ~/Desktop/minimize -type f) ; do ${scm} 0444 ${f} ; done	
+	${scm} a+x ~/Desktop/minimize/${distro}/*.sh
 
-	for f in $(find ~/Desktop/minimize/ -name '*.txt') ; do ${scm} a-wx ${f} ; done
-	for f in $(find ~/Desktop/minimize/ -name '*.conf') ; do ${scm} a-wx ${f} ; done
-	for f in $(find ~/Desktop/minimize/ -name 'conf') ; do ${scm} a-wx ${f} ; done
-	
-	if [ ${enforce} -eq 1 ] ; then #käyköhän jatkossa turhaksi tämä if-blokki?
+	if [ ${enforce} -eq 1 ] ; then #voisi tämä blokki kai olla siinä pre_enfoŕce'ssakin
 		echo "changing /sbin , /etc and /var 4 real"
 		${sco} -R root:root /sbin
 		${scm} -R 0755 /sbin
@@ -173,13 +150,14 @@ function enforce_access() {
 		#HUOM.080125:olikohan peräti tarpeellista että erikseen pre_e ja sitten tämä?		
 		for f in $(find /etc/sudoers.d/ -type f) ; do mangle2 ${f} ; done
 
+		#"find: ‘/etc/sudoers.d/’: Permission denied" jotain tarttis tehrä
 		for f in $(find /etc -name 'sudo*' -type f | grep -v log) ; do 
 			mangle2 ${f}
 			#csleep 1
 		done
 
-		#sudoersin sisältöä voisi kai tiukentaa kanssa
-		#${scm} 0755 /etc 
+		#sudoersin sisältöä voisi kai tiukentaa kanssa(?)
+		${scm} 0755 /etc 
 
 		#HUOM. 080125:tästgä saattaa tulla jotain nalkutusta
 		#pitäisi kai jotenkin huomioida:
@@ -188,7 +166,7 @@ function enforce_access() {
 		#0 drwxrwsr-x 2 root mail    3 Jul 20  2023 mail
 		
 		${sco} -R root:root /var
-		${scm} -R go-w /var #HUOM.260125: tässä go-w saattaa toimia, 0755 suattaa olla toimimatta   
+		${scm} -R 0755 /var #HUOM.260125: tässä go-w saattaa toimia, 0755 suattaa olla toimimatta   
 		
 		${sco} root:staff /var/local
 		${sco} root:mail /var/mail
@@ -196,12 +174,6 @@ function enforce_access() {
 		#jokohan alkaisi nalkutus loppua?
 		${sco} -R man:man /var/cache/man 
 		${scm} -R 0755 /var/cache/man
-
-		
-		${sco} root:staff /var/local
-		${sco} root:mail /var/mail
-		#ainakin chmod vielä... (TODO)
-
 
 		${scm} 0755 /
 		${sco} root:root /
@@ -213,12 +185,10 @@ function enforce_access() {
 	[ -f /etc/network/interfaces.${f} ] || ${spc} /etc/network/interfaces /etc/network/interfaces.${f}
 
 	if [ -s /etc/resolv.conf.new ] && [ -s /etc/resolv.conf.OLD ] ; then
-
 		${smr} /etc/resolv.conf 
 	fi
 
 	[ -s /sbin/dclient-script.OLD ] || ${spc} /sbin/dhclient-script /sbin/dhclient-script.OLD
-
 }
 
 #==================================PART 1============================================================
@@ -231,20 +201,16 @@ check_params
 [ ${enforce} -eq 1 ] && pre_enforce
 enforce_access 
 
-#exit
-
 dqb "man date;man hwclock; sudo date --set | sudo hwclock --set --date if necessary" 
 part1
 g=$(date +%F)
 
-
 #roiskisikohan nuo sources.list ja muut part1:seen?
-
 
 if [ -s /etc/apt/sources.list.tmp ] ; then #tämän kanssa tarttisi tehd vielä jotain?
 	dqb "https://raw.githubusercontent.com/senescent777/project/main/home/devuan/Dpckcer/buildr/bin/mutilate_sql_2.sh"
 	csleep 5
-
+	
 	[ -f /etc/apt/sources.list ] && sudo mv /etc/apt/sources.list /etc/apt/sources.list.${g}
 
 	sudo touch /etc/apt/sources.list
@@ -284,11 +250,9 @@ ${odio} /etc/init.d/ntpsec stop
 #K01avahi-jutut sopivaan kohtaan?
 
 #===================================================PART 2===================================
-
 #[ ${debug} -eq 1 ] && ${spd} > ${d}/pkgs-${g}.txt
 ##debug-syistä tuo yo. rivi
 #csleep 6
-
 
 ${sharpy} libblu* network* libcupsfilters* libgphoto* 
 # libopts25 ei tömmöistä daedaluksessa
@@ -303,10 +267,8 @@ ${sharpy} ntp*
 
 #uutena 050125, alunp. pol-paketit pois koska slahdot tammikuun -22 lopussa 
 ${sharpy} po* pkexec
-
 ${lftr}
 csleep 3
-
 
 if [ y"${ipt}" != "y" ] ; then 
 	${ip6tr} /etc/iptables/rules.v6
@@ -343,7 +305,6 @@ if [ ${mode} -eq 1 ] ; then
 	${odio} passwd
 	${whack} xfce* #HUOM. tässä ei tartte jos myöhemmin joka tap
 
-
 	exit 	
 fi
 
@@ -351,7 +312,6 @@ ${asy}
 dqb "GR1DN BELIALAS KYE"
 
 #VAIH:clouds uusix kanssa (case 1 vuelä)
-
 #katsotaan kanssa miten tuo uusi versio pelittää
 sudo ${d}/clouds.sh 0
 csleep 5
@@ -368,10 +328,8 @@ ${scm} a-wx $0 #kerta tulisi riittää
 #
 #csleep 6
 
-
 #===================================================PART 4(final)==========================================================
 #tulisi olla taas tables toiminnassa tässä kohtaa skriptiä
-
 
 if [ ${mode} -eq 2 ] ; then
 	echo "time to ${sifu} ${iface} or whåtever"
@@ -381,7 +339,6 @@ if [ ${mode} -eq 2 ] ; then
 fi
 
 sudo ${d}/clouds.sh 1
-
 
 #VAIH:stubby-jutut toimimaan
 #ongelmana error: Could not bind on given addresses: Permission denied
