e7cb3037ef9f5f94f4018775b79040d70d33293e
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-03-09 23:27:23 +0000

    script moved

---
+++ minimize/home/devuan/Desktop/minimize/demerde_toi.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#TODO: conf mukaan ... tai pikemminkin distro parametriksi
+tig=$(sudo which git)	
+csleep 1
+
+if [ x"${tig}" == "x" ] ; then
+	${shary} git
+	[ $? -eq 0 ] || exit 7
+fi
+
+csleep 5
+tig=$(sudo which git)
+
+q=$(mktemp -d)
+cd $q
+
+${tig} clone https://github.com/senescent777/some_scripts
+cd some_scripts/minimize
+
+if [ -d /home/devuan/Desktop/minimize ] ; then
+	mv ./home/devuan/Desktop/minimize/* /home/devuan/Desktop/minimize
+fi
+
+sudo chmod 0755 /home/devuan/Desktop/minimize/${distro}
+sudo chmod 0755 /home/devuan/Desktop/minimize/${distro}/*.sh
+cd /home/devuan/Desktop/minimize
+echo "./${distro}/export.sh 0 /tmp/vomit.tar"
