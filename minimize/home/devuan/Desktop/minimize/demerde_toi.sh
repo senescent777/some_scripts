a3b1eb607a7b2a328ed71a3f159d23980e600b17
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-03-10 00:12:41 +0000

    last commit before merge

--- minimize/home/devuan/Desktop/minimize/demerde_toi.sh
+++ minimize/home/devuan/Desktop/minimize/demerde_toi.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
-#TODO: conf mukaan ... tai pikemminkin distro parametriksi
+debug=1
+
+function dqb() {
+	[ ${debug} -eq 1 ] && echo ${1}
+}
+
+function csleep() {
+	[ ${debug} -eq 1 ] && sleep ${1}
+}
+
+if [ $# -gt 0 ] ; then
+	dqb "params_ok"
+else
+	echo "P.V.HH"
+	exit 66
+fi
+
 tig=$(sudo which git)	
 csleep 1
 
@@ -18,10 +34,10 @@ ${tig} clone https://github.com/senescent777/some_scripts
 cd some_scripts/minimize
 
 if [ -d /home/devuan/Desktop/minimize ] ; then
-	mv ./home/devuan/Desktop/minimize/* /home/devuan/Desktop/minimize
+	mv ./home/devuan/Desktop/minimize/* ~/Desktop/minimize
 fi
 
-sudo chmod 0755 /home/devuan/Desktop/minimize/${distro}
-sudo chmod 0755 /home/devuan/Desktop/minimize/${distro}/*.sh
-cd /home/devuan/Desktop/minimize
-echo "./${distro}/export.sh 0 /tmp/vomit.tar"
+sudo chmod 0755 ~/Desktop/minimize/${1}
+sudo chmod 0755 ~/Desktop/minimize/${1}/*.sh
+cd ~/Desktop/minimize
+echo "./${1}/export.sh 0 /tmp/vomit.tar"
