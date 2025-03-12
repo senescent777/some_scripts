065216bf182d1f0b3b50abf791ec21a73c19c644
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-03-12 23:30:24 +0000

    Add files via upload

--- minimize/home/devuan/Desktop/minimize/daedalus/lib.sh
+++ minimize/home/devuan/Desktop/minimize/daedalus/lib.sh
@@ -1,59 +1,5 @@
 #=================================================PART 0=====================================
 
-function pre_part3() {
-	[ y"${1}" == "y" ] && exit
-	echo "pp3( ${1} )"
-	[ -d ${1} ] || exit
-	echo "pp3.2"
-
-	#josko vielä testaisi löytyykö asennettavia ennenq dpkg	(esim find)
-	
-	${odio} dpkg -i ${1}/netfilter-persistent*.deb
-	[ $? -eq 0 ] && ${odio} shred -fu ${1}/netfilter-persistent*.deb
-	csleep 5
-
-	${odio} dpkg -i ${1}/libip*.deb
-	[ $? -eq 0 ] && ${odio} shred -fu ${1}/libip*.deb
-	csleep 5
-
-	${odio} dpkg -i ${1}/iptables_*.deb
-	[ $? -eq 0 ] && ${odio} shred -fu ${1}/iptables_*.deb
-	csleep 5
-
-	${odio} dpkg -i ${1}/iptables-*.deb
-	[ $? -eq 0 ] && ${odio} shred -fu ${1}/iptables-*.deb
-
-	dqb "pp3 d0n3"
-	csleep 5
-}
-
-pr4() {
-	dqb "pr4( ${1})"
-	csleep 5
-
-	${odio} dpkg -i ${1}/libpam-modules-bin_*.deb
-	${odio} dpkg -i ${1}/libpam-modules_*.deb
-	sudo shred -fu ${1}/libpam-modules*
-	csleep 5
-	${odio} dpkg -i ${1}/libpam*.deb
-
-	${odio} dpkg -i ${1}/perl-modules-*.deb
-	${odio} dpkg -i ${1}/libperl*.deb 
-	sudo shred -fu ${1}/perl-modules-*.deb 
-	sudo shred -fu ${1}/libperl*.deb
-	csleep 5
-
-	${odio} dpkg -i ${1}/perl*.deb
-	${odio} dpkg -i ${1}/libdbus*.deb
-	${odio} dpkg -i ${1}/dbus*.deb
-
-	sudo shred -fu ${1}/libpam*
-	sudo shred -fu ${1}/libperl*
-	sudo shred -fu ${1}/libdbus*
-	sudo shred -fu ${1}/dbus*
-	sudo shred -fu ${1}/perl*
-}
-
 function check_binaries() {
 	dqb "ch3ck_b1nar135()"
 	dqb "sudo= ${odio} "
@@ -63,33 +9,6 @@ function check_binaries() {
 	ip6t=$(sudo which ip6tables)
 	iptr=$(sudo which iptables-restore)
 	ip6tr=$(sudo which ip6tables-restore)
-
-	if [ y"${ipt}" == "y" ] ; then
-		echo "SHOULD INSTALL IPTABLES"
-		#konftdston muo9kkaaminen olisi ehkä helpompi
-		pre_part3 ~/Desktop/minimize/${distro}
-		pr4 ~/Desktop/minimize/${distro}
-
-		ipt=$(sudo which iptables)
-		ip6t=$(sudo which ip6tables)
-		iptr=$(sudo which iptables-restore)
-		ip6tr=$(sudo which ip6tables-restore)
-	fi
-
-	[ -x ${ipt} ] || exit 5
-	#jospa sanoisi ipv6.disable=1 isolinuxille ni ei tarttisi tässä säätää
-	[ -x ${ip6t} ] || exit 5
-	[ -x ${iptr} ] || exit 5
-	[ -x ${ip6tr} ] || exit 5
-
-	CB_LIST1="${ipt} ${ip6t} ${iptr} ${ip6tr} "
-	local x
-	
-	#passwd mukaan listaan?
-	for x in chown chmod pkill apt-get apt ip netstat dpkg ifup ifdown rm ln cp tar mount umount 
-		do ocs ${x} 
-	done
-
 	sco=$(sudo which chown)
 	scm=$(sudo which chmod)
 	whack=$(sudo which pkill)
@@ -104,24 +23,52 @@ function check_binaries() {
 	slinky=$(sudo which ln)
 	spc=$(sudo which cp)
 	srat=$(sudo which tar)
-
-	if [ ${debug} -eq 1 ] ; then
-		srat="${srat} -v "
-	fi
-
-	#HUOM. gpgtar olisi vähän parempi kuin pelkkä tar, silleen niinqu tavallaan
-
 	som=$(sudo which mount)
 	uom=$(sudo which umount)
 
 	dqb "half_fdone"
 	csleep 1
 
-	dch=$(find /sbin -name dhclient-script)
-	[ x"${dch}" == "x" ] && exit 6
-	[ -x ${dch} ] || exit 6
+	#TODO:ajamaan nuo komennot jatkossa
+	if [ y"${ipt}" == "y" ] ; then
+		echo "SHOULD INSTALL IPTABLES"
+		echo "pre_part3 ~/Desktop/minimize/${distro}"
+		echo "pr4 ~/Desktop/minimize/${distro}"
 
-	#HUOM:tulisi speksata sudolle tarkemmin millä param on ok noita komentoja ajaa
+		echo "ipt=\$(sudo which iptables)"
+		echo "ip6t=\$(sudo which ip6tables)"
+		echo "iptr=\$(sudo which iptables-restore)"
+		echo "ip6tr=\$(sudo which ip6tables-restore)"
+	fi
+
+	#HUOM. ocs()?
+	[ -x ${ipt} ] || exit 5
+	#jospa sanoisi ipv6.disable=1 isolinuxille ni ei tarttisi tässä säätää
+	[ -x ${ip6t} ] || exit 5
+	[ -x ${iptr} ] || exit 5
+	[ -x ${ip6tr} ] || exit 5
+	[ -x ${sco} ] || exit 5
+	[ -x ${scm} ] || exit 5
+	[ -x ${whack} ] || exit 5
+	[ -x ${sag} ] || exit 5
+	[ -x ${sa} ] || exit 5
+	[ -x ${sip} ] || exit 5
+	[ -x ${snt} ] || exit 5
+	[ -x ${sdi} ] || exit 5
+	[ -x ${sifu} ] || exit 5
+	[ -x ${sifd} ] || exit 5
+
+	[ -x ${smr} ] || exit 5
+	[ -x ${slinky} ] || exit 5
+	[ -x ${spc} ] || exit 5
+	[ -x ${srat} ] || exit 5
+	[ -x ${som} ] || exit 5
+	[ -x ${uom} ] || exit 5
+
+	#TODO:tulisi speksata sudolle tarkemmin millä param on ok noita komentoja ajaa
+	CB_LIST1="${ipt} ${ip6t} ${iptr} ${ip6tr} ${sco} ${scm} ${whack} ${sag} ${sa} ${sip} ${snt} ${sdi} ${sifu} ${sifd} ${smr} ${slinky} ${srat} ${spc} ${som} ${uom}"
+
+	dqb "spc= ${spc}"
 	dqb "b1nar135 0k" 
 	csleep 3
 }
@@ -137,45 +84,111 @@ function check_binaries2() {
 	whack="${odio} ${whack} --signal 9 "
 	snt="${odio} ${snt} "
 	sharpy="${odio} ${sag} remove --purge --yes "
-
-	spd="${odio} ${sdi} -l "
 	sdi="${odio} ${sdi} -i "
 
-	#HUOM. ${sag} OLTAVA VIIMEISENÄ NÄISTÄ KOLMESTA
+	#HUOM. ${sag} VIIMEISENÄ
 	shary="${odio} ${sag} --no-install-recommends reinstall --yes "
 	sag_u="${odio} ${sag} update "
 	sag="${odio} ${sag} "
 
 	sco="${odio} ${sco} "
 	scm="${odio} ${scm} "
-
-	dqb "${scm} a-wx ~/Desktop/minimize/*.sh in 5 secs"
-	csleep 5
-	${scm} a-wx /home/devuan/Desktop/minimize/*.sh
-	${scm} a-wx /home/devuan/Desktop/minimize/*.conf
-
 	sip="${odio} ${sip} "
 	sa="${odio} ${sa} "
 	sifu="${odio} ${sifu} "
 	sifd="${odio} ${sifd} "
-
 	smr="${odio} ${smr} "
-	lftr="${smr} -rf /run/live/medium/live/initrd.img* "
-
 	slinky="${odio} ${slinky} -s "
 	spc="${odio} ${spc} "
 	srat="${odio} ${srat} "
 	asy="${odio} ${sa} autoremove --yes"
-	fib="${odio} ${sa} --fix-broken install"
+
 	som="${odio} ${som} "
 	uom="${odio} ${uom} "	
-	dch="${odio} ${dch}"
+
+	smr="${odio} ${smr} "
+	lftr="${smr} -rf /run/live/medium/live/initrd.img* "
+
+#	dqb "spc= ${spc}"
 	dqb "b1nar135.2 0k.2" 
+
 	csleep 3
 }
+#
+#function mangle2() {
+#	if [ -f ${1} ] ; then 
+#		dqb "MANGLED ${1}"
+#		sleep 1
+#		${scm} o-rwx ${1}
+#		${sco} root:root ${1}
+#		csleep 1
+#	fi
+#}
+#
+##HUOM.220624:stubbyn asentumisen ja käynnistymisen kannalta sleep saattaa olla tarpeen
+#function ns2() {
+#	dqb "ns2( ${1} )"
+#
+#	${scm} u+w /home
+#
+#	${odio} /usr/sbin/userdel ${1}
+#	sleep 3
+#
+#	${odio} adduser --system ${1}
+#	sleep 1
+#	${scm} go-w /home
+#
+#	[ ${debug} -eq 1 ]  && ls -las /home
+#	sleep 7
+#}
+#
+#function ns4() {
+#	dqb "ns4( ${1} )"
+#
+#	${scm} u+w /run
+#	${odio} touch /run/${1}.pid
+#	${scm} 0600 /run/${1}.pid
+#	${sco} ${1}:65534 /run/${1}.pid
+#	${scm} u-w /run
+#
+#	sleep 5
+#	${whack} ${1}*
+#	sleep 5
+#
+#	dqb "starting ${1} in 5 secs"
+#
+#	sleep 5
+#	${odio} -u ${1} ${1} -g
+#	echo $?
+#	sleep 1
+#	pgrep stubby*
+#	sleep 5
+#}
+#
+#=========================PART 0 ENDS HERE=================================================================
+function pr4() {
+	echo "pr4 (${1})"
+	${odio} shred -fu ${1}/stubby*
+	${odio} shred -fu ${1}/libgetdns*
 
+	#uutena
+	${odio} shred -fu ${1}/dnsmasq*
+}
+
+#VAIH:jompaan kumpaan(pp3/pr4) dnsmasq* poisto (pidemmällä tähtäimellä tietty parempi laittaa toimimaanq poistaa mutta nyt näin)
+
+function pre_part3() {
+	dqb "pre_part3( ${1})"
+	${sdi} ${1}/dns-root-data*.deb
+	${smr} -rf ${1}/dns-root-data*.deb
+
+	#uutena, pois jos qsee	
+	${sdi} ${1}/perl-modules-*.deb
+	${smr} -rf ${1}/perl-modules-*.deb
+	
+}
+
+echo "BIL-UR-SAG"
 check_binaries
 check_binaries2
-
-
-
+echo "UMULAMAHRI"
