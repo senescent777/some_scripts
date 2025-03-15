8accff263af1c75c88bcfaf675c7534c66095b9f
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-03-15 23:10:21 +0000

    changes before testing pt 3

--- minimize/home/devuan/Desktop/minimize/daedalus/lib.sh
+++ minimize/home/devuan/Desktop/minimize/daedalus/lib.sh
@@ -182,7 +182,67 @@ function check_binaries() {
 #	dqb "b1nar135.2 0k.2" 
 #	csleep 3
 #}
-
+#function clouds_case0() {
+#	${slinky} /etc/resolv.conf.OLD /etc/resolv.conf
+#	${slinky} /etc/dhcp/dhclient.conf.OLD /etc/dhcp/dhclient.conf
+#	${spc} /sbin/dhclient-script.OLD /sbin/dhclient-script
+#
+#	if [ y"${ipt}" == "y" ] ; then
+#		dqb "SHOULD 1NSTALL TABL35"
+#	else
+#		${ipt} -A INPUT -p udp -m udp --sport 53 -j b 
+#		${ipt} -A OUTPUT -p udp -m udp --dport 53 -j e
+#
+#		for s in $(grep -v '#' /etc/resolv.conf | grep names | grep -v 127. | awk '{print $2}') ; do dda_snd ${s} ; done	
+#	fi
+#
+#	${odio} /etc/init.d/dnsmasq stop
+#	${odio} /etc/init.d/ntpsec stop
+#	csleep 5
+#	${whack} dnsmasq*
+#	${whack} ntp*
+#}
+#function clouds_case1() {
+#echo "WORK IN PROGRESS"
+#
+#		if [ -s /etc/resolv.conf.new ] ; then
+#			echo "r30lv.c0nf alr3ady 3x15t5"
+#		else
+#			sudo touch /etc/resolv.conf.new
+#			sudo chmod a+w /etc/resolv.conf.new
+#			sudo echo "nameserver 127.0.0.1" > /etc/resolv.conf.new
+#			sudo chmod 0444 /etc/resolv.conf.new
+#			sudo chown root:root /etc/resolv.conf.new
+#		fi
+#
+#		${slinky} /etc/resolv.conf.new /etc/resolv.conf
+#		${slinky} /etc/dhcp/dhclient.conf.new /etc/dhcp/dhclient.conf
+#		${spc} /sbin/dhclient-script.new /sbin/dhclient-script
+#
+#		if [ y"${ipt}" == "y" ] ; then
+#			echo "SHOULD 1NSTALL TABL35"
+#		else
+#			${ipt} -A INPUT -p tcp -m tcp --sport 853 -j b
+#			${ipt} -A OUTPUT -p tcp -m tcp --dport 853 -j e
+#			for s in $(grep -v '#' /home/stubby/.stubby.yml | grep address_data | cut -d ':' -f 2) ; do tod_dda ${s} ; done
+#		fi
+#
+#		echo "dns";sleep 2
+#		${odio} /etc/init.d/dnsmasq restart
+#		pgrep dnsmasq
+#
+#		echo "stu";sleep 2
+#		${whack} stubby* #090325: pitäisiköhän tämä muuttaa?
+#		sleep 3	
+#			
+#		[ -f /run/stubby.pid ] || sudo touch /run/stubby.pid
+#		${sco} devuan:devuan /run/stubby.pid #$n
+#		${scm} 0644 /run/stubby.pid 
+#		sleep 3
+#
+#		su devuan -c '/usr/bin/stubby -C /home/stubby/.stubby.yml -g'
+#		pgrep stubby
+#}
 check_binaries ${distro}
 check_binaries2
 
