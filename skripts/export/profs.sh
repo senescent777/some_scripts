e3c6ad9ca23790463d3c0594c5bc01efa0dad198
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-05-11 18:52:24 +0000

    Add files via upload

--- skripts/export/profs.sh
+++ skripts/export/profs.sh
@@ -1,9 +1,11 @@
-#netscape/mozilla/firefox profiles can be a Pain In the Ass
+#dqb "#i've known since year 2003 that netscape/mozilla/firefox profiles can be a Pain In the Ass"
+#csleep 6
+#lukkotiedostojen hävitys oli kanssa 1 juttu mikä piti uistaa tehdä...
 
 function cprof_1_1() {
 	#debug=1	
 	dqb "cprof1 ${1} ${2}"
-	csleep 5
+	csleep 3
 
 	local tmp
 	tmp=$(grep -c ${1} /etc/passwd)
@@ -14,14 +16,13 @@ function cprof_1_1() {
 			${smr} -rf /home/${1}/.mozilla 
 		fi
 	
-	
 		${odio} mkdir -p /home/${1}/.mozilla/firefox
 		${sco} -R ${1}:${1} /home/${1}/.mozilla/firefox
 	fi
 
 	if [ ${debug} -eq 1 ] ; then
-		echo "AFTER MKDIR";sleep 5
-		ls -las /home/${1}/.mozilla/firefox;sleep 6
+		echo "AFTER MKDIR";sleep 3
+		ls -las /home/${1}/.mozilla/firefox;sleep 3
 		echo "eEXIT CPROF_1_1($1)"
 	fi
 }
@@ -32,15 +33,16 @@ function cprof_1_2() {
 
 	fox=$(sudo which firefox)
 	local tmp
-	tmp=$(grep ${1} /etc/passwd | wc -l) #grep -c 
+	tmp=$(grep -c ${1} /etc/passwd)
 
 	if [ ${tmp} -gt 0 ] ; then 
 		if [ -x ${fox} ] ; then
 			cd /home/${1}
-			${odio} -u ${1} ${fox}&
+#			#${odio} -u ${1} toimisikohan ilmankn sudoa? kyl kait
+			${fox}&
 	
 			if [ $? -eq 0 ] ; then
-				sleep 5
+				sleep 3
 				${whack} firefox-esr 
 				${whack} firefox 
 			fi
@@ -49,13 +51,13 @@ function cprof_1_2() {
 		fi
 	fi
 
-	csleep 5
+	csleep 3
 }
 
 function cprof_1_3() {
 	#debug=1
 	dqb "cprof13 ${1} ${2} ${3}"
-	csleep 4
+	csleep 3
 	
 	[ -d /home/${2}/.mozilla/firefox ] || exit 68
 	[ x"${3}" == "x" ] && exit 69
@@ -64,9 +66,9 @@ function cprof_1_3() {
 	cd /home/${2}/.mozilla/firefox
 	
 	if [ ${debug} -eq 1 ] ; then
-		pwd;sleep 6 
-		echo "CPROF_1_3_1";sleep 5
-		ls -las /home/${2}/.mozilla/firefox;sleep 6
+		pwd;sleep 3
+		echo "CPROF_1_3_1";sleep 3
+		ls -las /home/${2}/.mozilla/firefox;sleep 3
 	fi
 
 	local tget
@@ -82,7 +84,7 @@ function cprof_1_3() {
 	if [ ${debug} -eq 1 ] ; then
 		echo -n "pwd=";pwd
 		echo "IN 6 SECONDS: sudo mv ${3}/* ."
-		sleep 6
+		sleep 3
 	fi
 
 	local f
@@ -90,24 +92,24 @@ function cprof_1_3() {
 	${sco} -R ${2}:${2} ./* 		
 	
 	if [ ${debug} -eq 1 ] ; then
-		echo "AFT3R MV";sleep 6
-		ls -las;sleep 5
+		echo "AFT3R MV";sleep 3
+		ls -las;sleep 3
 	fi	
 
-	csleep 5
+	csleep 3
 	dqb "CPROF13 D0N3"
 }
 
 function cprof_2_1() {
 	#debug=1
 	dqb "CPFOR21 ${1} , ${2}"
-	csleep 3
+	csleep 2
 
 	if [ x"${1}" != "x" ] ; then 
 		#${sco} -R ${1}:${1} /home/${1} #kommentteihin koska x
 		#voisi mennä ilmankin sudoa tuossa alla...
 		dqb "shdgfsdhgfsdhgf"
-		csleep 3
+		csleep 2
 
 		if [ -d /home/${1}/.mozilla ] ; then 
 			${sco} -R ${1}:${1} /home/${1}/.mozilla
@@ -122,13 +124,13 @@ function cprof_2_1() {
 	fi
 
 	dqb "d0n3"
-	csleep 3
+	csleep 2
 }
 
-function copyprof() { #TODO:import_prof jatkossa
+function imp_prof() { 
 	#debug=1
 	dqb "cprof ${1} ${2} ${3}"
-	csleep 3
+	csleep 2
 	cd /home/${2} 
 
 	if [ x"${2}" != "x" ] ; then 
@@ -137,14 +139,13 @@ function copyprof() { #TODO:import_prof jatkossa
 
 			cprof_1_1 ${2}
 			cprof_1_2 ${2}
-			
 			cprof_1_3 ${1} ${2} ${3}
 			cprof_2_1 ${2}
 		fi
 	fi
 
 	dqb "cpforf dnoe"
-	csleep 3
+	csleep 2
 }
 
 function exp_prof() {
@@ -153,8 +154,7 @@ function exp_prof() {
 	local p
 	local f
 	
-	csleep 3
-	
+	csleep 2
 	tget=$(ls ~/.mozilla/firefox/ | grep ${2} | tail -n 1)
 	p=$(pwd)
 
@@ -172,6 +172,6 @@ function exp_prof() {
 	#*.js ja *.json kai oleellisimmat kalat
 	cd ${p}
 
-	csleep 5
+	csleep 2
 	dqb "eprof.D03N"
 }
