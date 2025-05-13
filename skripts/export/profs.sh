210726568364d6f65cba577bd80f966ead95d73c
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-05-13 23:54:25 +0300

    Add files via upload

--- skripts/export/profs.sh
+++ skripts/export/profs.sh
@@ -1,12 +1,9 @@
 #dqb "#i've known since year 2003 that netscape/mozilla/firefox profiles can be a Pain In the Ass"
 #csleep 6
 #lukkotiedostojen hävitys oli kanssa 1 juttu mikä piti uistaa tehdä...
-
-function cprof_1_1() {
-	#debug=1	
+function oldprof() {
 	dqb "cprof1 ${1} ${2}"
 	csleep 3
-
 	local tmp
 	tmp=$(grep -c ${1} /etc/passwd)
 
@@ -19,18 +16,10 @@ function cprof_1_1() {
 		${odio} mkdir -p /home/${1}/.mozilla/firefox
 		${sco} -R ${1}:${1} /home/${1}/.mozilla/firefox
 	fi
-
-	if [ ${debug} -eq 1 ] ; then
-		echo "AFTER MKDIR";sleep 3
-		ls -las /home/${1}/.mozilla/firefox;sleep 3
-		echo "eEXIT CPROF_1_1($1)"
-	fi
 }
 
-function cprof_1_2() {
-	#debug=1
+function createnew() {
 	dqb "cpfor_12 ${1},${2}"
-
 	fox=$(sudo which firefox)
 	local tmp
 	tmp=$(grep -c ${1} /etc/passwd)
@@ -53,61 +42,44 @@ function cprof_1_2() {
 
 	csleep 3
 }
-
-function cprof_1_3() {
-	#debug=1
+function copy_to() {
+	debug=1
 	dqb "cprof13 ${1} ${2} ${3}"
 	csleep 3
-	
+
 	[ -d /home/${2}/.mozilla/firefox ] || exit 68
 	[ x"${3}" == "x" ] && exit 69
 	[ -d ${3} ] || exit 70
 
-	cd /home/${2}/.mozilla/firefox
-	
-	if [ ${debug} -eq 1 ] ; then
-		pwd;sleep 3
-		echo "CPROF_1_3_1";sleep 3
-		ls -las /home/${2}/.mozilla/firefox;sleep 3
-	fi
-
 	local tget
-	tget=$(ls | grep ${1} | tail -n 1) 
+	tget=$(find /home/${2}/.mozilla/firefox -type d | grep -v '+' | grep ${1} | head -n 1)
+	dqb "TGET= ${tget}"
 
-	${sco} ${2}:${2} ./${tget}
-	${scm} 0700 ./${tget}
-		
-	if [ x"${tget}" != "x" ] ; then 
-		cd ${tget}
-	fi
+	${sco} ${2}:${2} ${tget}
+	${scm} 0700 ${tget}
 
-	if [ ${debug} -eq 1 ] ; then
-		echo -n "pwd=";pwd
-		echo "IN 6 SECONDS: sudo mv ${3}/* ."
-		sleep 3
-	fi
+	dqb "IN 3 SECONDS: sudo mv ${3}/* ${tget}"
+	csleep 3
 
 	local f
-	for f in $(find ${3} -type f -name '*.js*') ; do mv ${f} . ; done
+	for f in $(find ${3} -type f -name '*.js*') ; do mv ${f} ${tget} ; done
 	${sco} -R ${2}:${2} ./* 		
 	
 	if [ ${debug} -eq 1 ] ; then
 		echo "AFT3R MV";sleep 3
-		ls -las;sleep 3
+		ls -las ${tget}
+		sleep 3
 	fi	
 
 	csleep 3
 	dqb "CPROF13 D0N3"
 }
 
-function cprof_2_1() {
-	#debug=1
+function access() {
 	dqb "CPFOR21 ${1} , ${2}"
 	csleep 2
 
 	if [ x"${1}" != "x" ] ; then 
-		#${sco} -R ${1}:${1} /home/${1} #kommentteihin koska x
-		#voisi mennä ilmankin sudoa tuossa alla...
 		dqb "shdgfsdhgfsdhgf"
 		csleep 2
 
@@ -128,19 +100,17 @@ function cprof_2_1() {
 }
 
 function imp_prof() { 
-	#debug=1
 	dqb "cprof ${1} ${2} ${3}"
 	csleep 2
-	cd /home/${2} 
 
 	if [ x"${2}" != "x" ] ; then 
 		if [ -d /home/${2} ] ; then 
 			${scm} 0700 /home/${2}
 
-			cprof_1_1 ${2}
-			cprof_1_2 ${2}
-			cprof_1_3 ${1} ${2} ${3}
-			cprof_2_1 ${2}
+			oldprof ${2}
+			createnew ${2}
+			copy_to ${1} ${2} ${3}
+			access ${2}
 		fi
 	fi
 
@@ -169,7 +139,6 @@ function exp_prof() {
 
 	${srat} -cvf ${1} ./rnd
 	for f in $(find . -name '*.js') ; do ${srat} -rf ${1} ${f} ; done
-	#*.js ja *.json kai oleellisimmat kalat
 	cd ${p}
 
 	csleep 2
