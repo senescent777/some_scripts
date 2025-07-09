e7f7db9348ec9b37b67e58b8c88070deef72a70d
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-07-09 18:38:58 +0300

    few more pkgs added

--- skripts/init.sh
+++ skripts/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo nano /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get reinstall git squashfs-tools
+sudo apt-get reinstall git squashfs-tools gpg gpgv 
+sudo apt-get reinstall genisoimage #xorriso
 sudo tar -cvf ${1} /var/cache/apt/archives/*.deb
