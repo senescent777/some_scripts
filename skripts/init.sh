cd02cdf0d54677ed288da466d509c86f6015f67a
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-07-09 19:58:47 +0300

    grub-mkrescue

--- skripts/init.sh
+++ skripts/init.sh
@@ -3,7 +3,9 @@ sudo nano /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get reinstall git squashfs-tools gpg gpgv #vaiko gpg*
 sudo apt-get reinstall genisoimage wodim 
-sudo apt-get reinstall grub* #xorriso
+
+#https://pkginfo.devuan.org/cgi-bin/package-query.html?c=package&q=grub-common=2.06-13+deb12u1
+sudo apt-get reinstall grub-common xorriso #jälkimminen toistaiseksi mukana
 sudo tar -cvf ${1} /var/cache/apt/archives/*.deb
 
 
