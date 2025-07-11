6b5318c3f36e2591a7c4f68652c1a40201b337cd
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-07-11 22:22:16 +0000

    init-dev-env-related

--- skripts/init.sh
+++ skripts/init.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-sudo nano /etc/apt/sources.list
+
+if [ -s $0.conf ] ; then
+	. $0.conf 
+fi
+
+if [ ! -s ${basedir}/sources.list ] ; then
+	sudo nano /etc/apt/sources.list #tai cp
+else
+	if [ ! -s /etc/apt/sources.list.old ] ; then
+		sudo mv /etc/apt/sources.list /etc/apt/sources.list.old
+		sudo cp ${basedir}/sources.list /etc/apt/
+	fi
+fi
+
 sudo apt-get update
 
 sudo apt-get reinstall git squashfs-tools gpg gpgv #vaiko gpg*
@@ -7,10 +20,18 @@ sudo apt-get reinstall genisoimage wodim
 
 #https://pkginfo.devuan.org/cgi-bin/package-query.html?c=package&q=grub-common=2.06-13+deb12u1
 sudo apt-get reinstall grub-common xorriso #jälkimminen toistaiseksi mukana
-sudo tar -cvf ${1} /var/cache/apt/archives/*.deb #tai sit cp
+
+if [ ! -d ${1} ] ; then
+	sudo tar -cvf ${1} /var/cache/apt/archives/*.deb
+else
+	sudo cp  /var/cache/apt/archives/*.deb ${1}
+fi
 
 chmod a+x ./*.sh
 
+[ -s ${basedir}/.gitignore ] || touch ${basedir}/.gitignore
+c=$(grep $0.conf ${basedir}/.gitignore | wc -l)
+[ ${c} -lt 1 ] && echo $0.conf >> ${basedir}/.gitignore
 
 #TODO:konftsdton mukaisia hmistoja tulisi luoda jos ei jo olemassa?
 
--- skripts/init2.sh
+++ skripts/init2.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-#d=$(dirname $0) #tarvitseeko?
-
 if [ -s $0.conf ] ; then
-	. $0.conf 
+	. $0.conf
+else
+	exit 666
 fi
 
-#common_lib.part3()
-echo "sudo dpkg -i ${pkgsrc}/*.deb"
+#common_lib.part3() ... joutuu ehkä jo järjestyksen kanssa säätämään
+sudo dpkg -i ${pkgsrc}/*.deb
 
 #git olemassaolo pitäisi testata ennernq etenee pidemmälle
-[ -v ue ] && echo "git config --global user.email ${ue}"
-[ -v un ] && echo "git config --global user.name ${un}"
+[ -v ue ] && git config --global user.email ${ue}
+[ -v un ] && git config --global user.name ${un}
 
-if [ -s $0.conf ] ; then
-	if [ ! -s ${basedir}/.gitignore ] ; then
-		echo "echo $0.conf >> ${basedir}/.gitignore "
-	fi
-fi
+#VAIH:grepin kanssa jatkossa testaus
+#if [ -s $0.conf ] ; then
+#	if [ ! -s ${basedir}/.gitignore ] ; then
+#		echo $0.conf >> ${basedir}/.gitignore
+#	fi
+#fi
+[ -s ${basedir}/.gitignore ] || touch ${basedir}/.gitignore
+c=$(grep $0.conf ${basedir}/.gitignore | wc -l)
+[ ${c} -lt 1 ] && echo $0.conf >> ${basedir}/.gitignore
