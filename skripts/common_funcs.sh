8834acdc72b1b622079d6db552e985f6a26d4370
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-10-12 11:17:59 +0000

    changes of 12.10.25
    - should consider merging after i have fixed commandline parsing

--- skripts/common_funcs.sh
+++ skripts/common_funcs.sh
@@ -1,3 +1,4 @@
+#TODO:ce common_lib.sh includeointi
 odio=$(which sudo) 
 #TODO:sudoon liittyen se sudoers-meshuggah-jekku käyttöön myös näihin remasterointiskripteihimn, varm. vuoksi
 #========================tilapäisesti tässä, common_lib myöh==
@@ -50,46 +51,65 @@ function csleep() {
 
 #TODO:tähän asti juttujen korvaaminen toisen projektin common_lib
 #============================================
+#
+##se uusi gpo() tilalle?
+##VAIH:VÄHITELLEN SE "-v"-OPTIO QNTOON
+#function parse_opts() {
+#
+#	dqb "parse_opts(${1}, ${2})"
+#
+#	local common_pars
+#	common_pars=0
+#
+#	#TODO:--bl mukaan yelisiin optioihin?
+#	if [ x"$1" != "x" ] ; then 
+#		if [ x"$2" != "x" ] ; then
+#			case ${1} in
+#				-v|-V|--v|--V)
+#					common_pars=1
+#				;;
+#				*)
+#					parse_opts_real ${1} ${2}
+#				;;
+#			esac
+#		else
+#			case ${1} in
+#				-h|--h)
+#					common_pars=1
+#					usage
+#				;;
+#				*)
+#					single_param ${1}
+#				;;		
+#			esac
+#		fi
+#
+#		if [ x"${cmd}" != "x" ] ; then
+#			echo "1"
+#		else
+#			if [ ${common_pars} -eq 0 ] ; then
+#				echo 0
+#				cmd=${1}
+#			fi
+#		fi
+#	fi
+#}
 
-#se uusi gpo() tilalle?
-#TODO:VÄHITELLEN SE "-v"-OPTIO QNTOON
-function parse_opts() {
+#====================common_lib.sh==================================
+function gpo() {
+	dqb "GPO"
+	#getopt olisi myös keksitty
 
-	dqb "parse_opts(${1}, ${2})"
+	local prevopt
+	local opt
+	prevopt=""
 
-	local common_pars
-	common_pars=0
-
-	#TODO:--bl mukaan yelisiin optioihin?
-	if [ x"$1" != "x" ] ; then 
-		if [ x"$2" != "x" ] ; then
-			case ${1} in
-				-v|-V|--v|--V)
-					common_pars=1
-				;;
-				*)
-					parse_opts_real ${1} ${2}
-				;;
-			esac
-		else
-			case ${1} in
-				-h|--h)
-					common_pars=1
-					usage
-				;;
-				*)
-					single_param ${1}
-				;;		
-			esac
-		fi
-
-		if [ x"${cmd}" != "x" ] ; then
-			echo "1"
-		else
-			if [ ${common_pars} -eq 0 ] ; then
-				echo 0
-				cmd=${1}
-			fi
-		fi
-	fi
+	for opt in $@ ; do
+		parse_opts_1 ${opt}
+		parse_opts_2 ${prevopt} ${opt}
+		prevopt=${opt}
+	done
 }
+
+gpo "$@"
+#====================common_lib.sh==================================
