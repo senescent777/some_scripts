c2ca1895a946722a89bcfd49e4a51f410a7929d0
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-12-16 18:09:17 +0000

    Add files via upload

--- skripts/mksums_new.sh
+++ skripts/mksums_new.sh
@@ -11,29 +11,28 @@ function usage() {
 	echo "$0 --in <source> [--bl <BLOADER>]"
 	echo "$0 --iso"
 	echo "$0 --pkgs"
-	echo "$0 -h" #voisi olla vakiovaruste tuo optio ja liityvä fktio
+	echo "$0 -h" #voisi olla vakiovaruste tuo optio ja liityvä fktio (jos vähän jo 111225 siihen suuntaan)
 	exit 44
 }
 
-#miten se -v?
 function parse_opts_real() {
 	dqb "asd.asd"
 }
 
-if [ -f ${d}/keys.conf ] ; then #tarcitaan, kts sibgle_param
+if [ -f ${d}/keys.conf ] ; then #tarvitaan, kts sibgle_param
 	. ${d}/keys.conf
 fi
 
-#141025:toiminee avainten asennuksne jälkeen
+#141025:ainakin silloin taisi toimia avainten asennuksen jälkeen
 function single_param() {
 	case ${1} in
 		--iso)
-			[ -v CONF_kay2name ] || exit 68
-			${gg} -u ${CONF_kay2name} -sb ./*.iso
+			[ -v CONF_ksk ] || exit 68
+			${gg} -u ${CONF_ksk} -sb ./*.iso
 			exit 61
 		;;
 		--pkgs)
-			[ -v CONF_kay2name ] || exit 68
+			[ -v CONF_ksk ] || exit 68
 			[ -v CONF_pkgsdir2 ] || exit 67
 			[ -v CONF_BASEDIR ] || exit 66
 			[ x"${CONF_BASEDIR}" != "x" ] || exit 65
@@ -41,8 +40,8 @@ function single_param() {
 
 			cd ${CONF_BASEDIR}/${CONF_pkgsdir2}
 
-			${gg} -u ${CONF_kay2name} -sb ./*.deb
-			[ $? -eq 0 ] && ${gg} -u ${CONF_kay2name} -sb ./*.bz2
+			${gg} -u ${CONF_ksk} -sb ./*.deb
+			[ $? -eq 0 ] && ${gg} -u ${CONF_ksk} -sb ./*.bz2
 			
 			exit 63
 		;;
@@ -56,13 +55,13 @@ if [ $# -eq 0 ] ; then
 fi
 
 function part0() {
-	dqb "part0( ${1})"
+	dqb "part0( ${1}, ${2})"
 	csleep 1
 
 	[ -z ${1} ] && exit 65
 	[ -d ${1} ] || exit 67
 
-	[ -z ${2} ] && exit 69 #pitäisikö lisäksi grepata /e/passwd ?
+	[ -z ${2} ] && exit 69
 	[ "${1}" == "/" ] && exit 71	
 	
 	grep ${2} /etc/passwd
@@ -82,6 +81,9 @@ function part0() {
 		rm ${f}
 	done
 
+	[ ${debug} -eq 1 ] && ls -las ${1}/${TARGET_DIGESTS_file}*
+	csleep 3
+
 	local i
 	for i in ${MKS_parts} 4 ;  do touch ${1}/${TARGET_DIGESTS_file}.${i} ; done
 	
@@ -89,12 +91,13 @@ function part0() {
 	csleep 1
 }
 
+#HUOM.281125:pad alla julk avaimet .bz2:sessa olisi idea
 function part123() {
 	#debug=1
 	dqb "part123(${1}, ${2} , ${3} )"
 
 	[ z"${1}" != "z" ] || exit 111
-	#[ -d ${2} ] || exit 112 #miksi kommenteissa?
+	#[ -d ${2} ] || exit 112 #miksi kommenteissa? testaa syy?
 	[ -d ${3} ] || exit 113
 
 	local old
@@ -105,24 +108,22 @@ function part123() {
 		cd ${3}
 		[ ${debug} -eq 1 ] && pwd
 		dqb "find ./${2} -type f"
-		csleep 1
+		csleep 3
 
-		#helpompi bain hakea .cfg-päätteiset? tai kts grubin osalta se manuaali että mitkä aiheellisia
-		#| grep -v ${TARGET_patch_name}
+		#HUOM.281125:saattaa joutua muuttamaan vielä jos isolinuxin kanssa alkaa säätää
+		for f in $(find ./${2} -type f -name "*.cfg" -or -name "*.lst" -or -name "grubenv") ; do ${sah6} ${f} >> ./${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${1} ; done
+		for f in $(find ./${2} -type f -name "*.mod" -or -name "vmlinuz*" -or -name "initrd*") ; do ${sah6} ${f} >> ./${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${1} ; done
+		for f in $(find ./${2} -type f -name "*.bz2" -or -name "filesystem*") ; do ${sah6} ${f} >> ./${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${1} ; done
 
-		#-name "*.cfg" -or -name "*.lst" -or \
-  		#-name "*.mod" -or -name "vmlinuz*" -or -name "initrd*" -or \
-  		#-name "grubenv"`;
-
-		for f in $(find ./${2} -type f | grep -v ${TARGET_DIGESTS_file0} | grep -v boot.cat | grep -v isolinux.bin | grep -v '.mod' | grep -v '.c32') ; do
-			dqb "${sah6} ${f}"
-			${sah6} ${f} >> ./${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${1}			
-		done
+		#HUOM.siinä aiemmassa virityksessä taisi tulla myös devuan.conf (tjsp) mukaan
+		#... ja ne 2 julk av myös
+		for f in $(find ./${2} -type f -name "*.conf")  ; do ${sah6} ${f} >> ./${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${1} ; done
 
 		${scm} 0444 ./${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${1}
-		#${sco} 0:0 ./${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${1}
+		#${sco} 0:0 ./${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${1} #ei hyvä idea?
 		cd ${old}
 	else
+		#dgsts.4 luonti ei onnistu?
 		dqb "ERROR TERROR"
 	fi
 
@@ -130,7 +131,6 @@ function part123() {
 }
 
 function part456() {
-	#debug=1
 	dqb "part456 $1 ; $2 ; $3"
 	[ z"${1}" != "z" ] || exit 666
 	
@@ -144,70 +144,58 @@ function part456() {
 	fi
 }
 
-#HUOM.kandee ajaa vain jos binäärit ja avaimet olemassa
+#HUOM.kandee ajaa tämä vain jos binäärit ja avaimet olemassa
+#161225:miten parametrit nykyään? mitä tulee ja mitä tarvitaan? jos MKS_PARTS parametriksi?
 function part6_5() {
-	dqb "part65( ${1}, ${2}, ${3})"
-	local i
-	pwd
+	dqb "mks.part65 ${1} , ${2} , ${3} "
+
+	[ -v CONF_pubk ] || exit 99	
+	[ -v TARGET_DIGESTS_dir ] || exit 98
+	[ -v TARGET_DIGESTS_file ] || exit 97
 	csleep 1
 
+	[ ${debug} -gt 0 ] && pwd
+	csleep 1
+	local i
+
 	for i in ${MKS_parts} ; do
-		dqb "${gg} -u ${CONF_kay1name} -sb ./${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${i}"
-		${gg} -u ${CONF_kay1name} -sb ./${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${i}
-		[ $? -gt 0 ] && dqb "install-keys --i ?"
+		dqb "${gg} -u ${CONF_pubk} -sb ./${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${i}"
+		${gg} -u ${CONF_pubk} -sb ./${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${i}
+		[ $? -gt 0 ] && dqb "install-keys --i | --j ?"
 	done
 
 	dqb "dibw"
 }
 
+#151225:avainten allek ja const:it ok, pitää vain kopsata kohdehak alle jossain sopivassa kohdassa(TODO)
+#TODO:target_dpub-jutut pois sittenq mahd
 function part7() {
-	dqb "part7"	
-	[ ${debug} -eq 1 ] && pwd
-	csleep 2
+	dqb "mks.part7 ( ${1} , ${2} , ${3} ) " #mitvit taas
 
-	dqb "${gg} -u ${CONF_kay2name} -sb ./${TARGET_Dpubkf}"
-	${gg} -u ${CONF_kay2name} -sb ./${TARGET_Dpubkf}
+	[ -v TARGET_DIGESTS_dir ] || exit 98
+	[ -v TARGET_DIGESTS_file ] || exit 97	
+	[ ${debug} -gt 0 ] && pwd
+	csleep 1
 
-	[ $? -gt 0 ] && dqb "install-keys --i ?"
-	csleep 2
+	dqb "${gg} -u ${CONF_ksk} -sb ./${TARGET_Dpubkf}"
+	${gg} -u ${CONF_ksk} -sb ./${TARGET_Dpubkf}
+	[ $? -gt 0 ] && dqb "install_keys  ?"
+	csleep 5
 
 	${gg} --verify ./${TARGET_Dpubkf}.sig
-	[ $? -gt 0 ] && dqb "install-keys --i ?"
+	[ $? -gt 0 ] && dqb "install_keys  ?"
+	csleep 5
+
 	local i
 
 	for i in ${MKS_parts} ; do
-		#${gv} --keyring ./${TARGET_Dpubkf} ${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${i}.sig ${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${i}
 		${gg} --verify ${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.${i}.sig 
+		csleep 1
 	done
 
 	echo $?
 	csleep 1
 	dqb "p7 done"
-}
-
-function part8() {
-	dqb "p8 ${1}"
-	[ x"${TARGET_patch_name}" != "x" ] || exit 665
-	[ x"${1}" != "x" ] || exit 665
-
-	dqb "params ok"		
-
-	local olddir=$(pwd)	
-	cd ../out
-
-	case ${1} in
-		1)
-			echo $?
-		;;
-		2)
-			dqb "2"
-		;;
-		*)
-			dqb "default"
-		;;
-	esac 
-
-	cd ${olddir}
 }
 
 [ -d ${source} ] || exit 99
@@ -235,14 +223,18 @@ csleep 5
 dqb "BOOTLEREOD ONEDD"
 csleep 1
 
-#jotebkin toisin jatkossa? hakemistoa kohti 1 tdsto ja täts it? eio erikseen 1,2,3-juttui
+#jotebkin toisin jatkossa? hakemistoa kohti 1 tdsto ja täts it? ei erikseen 1,2,3-juttui
+#part123() nykyisellään ei tee .sh-tiedostoista tsummia, mutta jos pakkaisi bz2:seen... (liittyen:"exp2 c" voisi muuttaa)
 part123 2 ${TARGET_pad_dir} ${source}
 part123 3 live ${source}
 
+#161225:dgsts.1 kanssa jotain? yhtäkkiä yli 100k
 cd ${source}
 for p in ${MKS_parts} ; do part456 ${p}; done
 
-#HUOM.30725:jokohan alkaisi jo avainjutut toimia
+#kts liittyen jlk_main() , että mitä pitäisi sisällöksi laittaa, esim
+#niinja nw julk av pitäisi myös tulla mukaan, kts stage0_backend liittyen
+
 if [ x"${gg}" != "x" ] ; then 
 	part6_5
 fi
@@ -250,16 +242,15 @@ fi
 part7 
 [ ${debug} -eq 1 ] && pwd
 
-part8 ${b}
+#part8 ${b}
 [ ${debug} -eq 1 ] && pwd
 
-#TODO:$sc-juttujen kanssa sudoersiin rajoitus jos mahd
 ${sco} ${n}:${n} ./${TARGET_DIGESTS_dir}/* 
 ${scm} 0644 ./${TARGET_DIGESTS_dir}/* 
 [ ${debug} -eq 1 ] && ls -las ./${TARGET_DIGESTS_dir}
 csleep 1
 
-#HUOM.18726: dgsts.4 kanssa myös jotain jurpoilua? vielä 031025 ? joo (tee jotain)
+#271125:josko ao. blokki jo kunnossa?
 dqb "${sah6} ./${TARGET_DIGESTS_dir}/* | grep -v '${TARGET_DIGESTS_file}.4' | grep -v 'cf83e' | head -n 10" # | grep -v 'SAM' turha?
 ${sah6} ./${TARGET_DIGESTS_dir}/* | grep -v '${TARGET_DIGESTS_file}.4' | grep -v 'cf83e' | head -n 10 > ./${TARGET_DIGESTS_dir}/${TARGET_DIGESTS_file}.4
 part456 4
