9b91921d4cadc0fd3feee165ce88fc2cbe2bffec
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-12-16 17:36:18 +0000

    bypassing merge.1
    hope this still works

--- stage0.sh
+++ stage0.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 debug=0
 . ./skripts/common.conf
-base=""
+source=""
 source2=""
-bl=${CONF_bloader}
+bl=${CONF_bloader} #tähän liittyen oli se juttu toisessa repossa mikö pitäisi
+cmd=""
 
 if [ $# -lt 1 ] ; then
 	echo "$0 -h"
@@ -11,58 +12,44 @@ if [ $# -lt 1 ] ; then
 fi
 
 function usage() {
-	echo "${0} --base <BASE> --add <THINGS_TO_ADD> [--bl BLOADER] [--v <verbosity_level>]"
-	echo "${0} --get-devuan <download_dir>"
+	echo "${0} --in <BASE> --add <THINGS_TO_ADD> [--bl BLOADER] [--v <verbosity_level>]"
+	#echo "${0} --get-devuan <download_dir>"
 	echo "${0} -d Destroys contents of ${CONF_tmpdir}"
 	echo "${0} --make-dirs creates some dirs under ${CONF_tmpdir}"
 }
 
-#151025:miten nykyään -v:n kanssa?
-. ./skripts/stage0_backend.bsh
+function single_param() {
+	dqb "subgle(${1} , ${2})"
+	#-v ei tarttisi kelpuuttaa tässä
+	[ "${1}" == "-v" ] || cmd=${1}
+}
 
+##... koita päättää mitä tuo $1 edustaa
+#function get_devuan() {
+#	echo "cd ${1}"
+#	echo "wget ${CONF_wget_opts} ${1}"
+#	echo "sha256sum -c SHA256SUMS"
+#	[ $? -eq 0 ] || echo "https://www.youtube.com/watch?v=PjotFePip2M"
+#}
+
+#BTW. "-d -v" miten se hoidetaan?
 function parse_opts_real() {
 	dqb "douböe(${1} , ${2})"
 
 	case ${1} in
-		--base) #--in ,--out yleisiksi param jrkossa?
-			base=${2}
-		;;
 		--add)
 			source2=${2}
 		;;
-		--get-devuan)
-			get_devuan ${2}
-			exit
-		;;
+#		--get-devuan)
+#			#jatkossa kai main() kautta jos tarpeen
+#			get_devuan ${2}
+#			exit
+#		;;
 	esac	
 }
 
-function single_param() {
-	dqb "subgle(${1} , ${2})"
-
-	case ${1} in
-		--make-dirs)
-			#init.bash käskyttämään tätä case:a tarvittaessa?
-			make_src_dirs ${CONF_bloader}
-			make_tgt_dirs
-			exit
-		;;
-		-d)
-			[ -v CONF_tmpdir ] || exit 68
-
-			if [ x"${CONF_tmpdir}" != "x" ] ; then 
-				echo "${smr} -rf ${CONF_tmpdir}/* IN 6 SECS";sleep 6	
-				${smr} -rf ${CONF_tmpdir}/*
-			fi
-
-			exit
-		;;
-	esac
-
-	#exit 13
-}
-
 . ./skripts/common_funcs.sh
+. ./skripts/stage0_backend.bsh
 
 if [ -d ${CONF_tmpdir0} ] ; then
 	dqb "CONF_TMPDIR0 EXISTS"
@@ -71,6 +58,52 @@ else
 	exit 7
 fi
 
-#stage0f==glorified cp
-#dqb "mkdir -p ./v/smthing;mkdir -p ./v/smthing/{isolinux,grub};ln -s ~/Desktop/minimize ./v/something/pad ?"
-echo "./stage0f.sh ${base} ${source2} ${bl} ${debug}"
+dqb "${cmd}"
+csleep 1
+
+#main()
+#TODO:sitä sudoersin sorkkimista sietäisi muuttaa koska viimeaikaiset ... kantava idea oli mikä?
+#... CB_LIST2 liittyikö?
+
+case ${cmd} in
+	--make-dirs)
+		#init.bash käskyttämään tätä case:a tarvittaessa?
+		make_src_dirs ${CONF_bloader}
+
+		#onkohan mieltä tehdä noin päin kuin alla?
+		make_tgt_dirs ${CONF_target} ${CONF_source} ${CONF_bloader}
+		exit
+	;;
+	-d)
+		[ -v CONF_tmpdir ] || exit 68
+		[ -z ${CONF_tmpdir} ] && exit 69
+		[ "${CONF_tmpdir}" == "/" ] && exit 70
+
+		dqb "CONF_tmp maybe ok"
+		csleep 1
+
+		#TODO:man chattr pitkästä aikaa
+		#081225:v-hmiston alta jotain siivoilua myös? no ei
+		#VAIH:josko jo sudon pudotus smr:stä tai sittense sudoers
+		#smr=$(which rm)
+		
+		dqb "smr= ${smr}"
+		csleep 2
+
+		if [ x"${CONF_tmpdir}" != "x" ] ; then 
+			echo "${smr} -rf ${CONF_tmpdir}/* IN 6 SECS";sleep 6	
+			${smr} -rf ${CONF_tmpdir}/*
+		fi
+
+		if [ ${debug} -gt 0 ] ; then
+			ls -las ${CONF_tmpdir} 
+			sleep 5
+		fi
+
+		exit
+	;;
+	*)
+		#stage0f==glorified cp
+		echo "./stage0f.sh ${source} ${source2} ${bl} ${debug}"
+	;;
+esac
--- stage0f.sh
+++ stage0f.sh
@@ -19,34 +19,30 @@ function single_param() {
 [ $# -gt 3 ] && debug=${4}
 . ./skripts/stage0_backend.bsh
 . ./skripts/common_funcs.sh
-#if [ -f ./skripts/keys.conf ] ; then #HUOM.141025:kts. copy_sums()
-#	. ./skripts/keys.conf
-#fi
 
-#VAIH:verbosity_level_jutut? vähitellen kunnssa?
+if [ -f ./skripts/keys.conf ] ; then #161225 laitettu takaisin syystä
+	. ./skripts/keys.conf
+fi
+
 dqb "PARAMS OK?"
-#echo "TEHTY?:isolubnux.cfg";sleep 5
 
-#TEHTY?:jotenkin kätevösti pitäisi saada menemään juttujen kopioituminen squash-hmiston alle
 #TODO:voisi olla jotain default-bootloader-konftdstoja jos ei v/$something alla ole
-#TODO:CONF_T parametriksi?
 #HUOM.12725:cp -a saattaisi olla fiksumpi kuin nämä kikkailut, graft-points vielä parempi
 function part0() {
 	#debug=1
-	dqb "PART0 ${1}, ${2} , ${3}"
+	dqb "stg0f.PART0 ${1}, ${2} , ${3} , ${4}"
 	pwd
 	csleep 2
 
 	dqb "COPY1NG FILES IN 1 SEC"
 	csleep 1
 
-	#ei aina tarttisi näiTä renkata
 	for f in ./filesystem.squashfs ./vmlinuz ./initrd.img ; do
 		if [ -s ${2}/live/${f} ] ; then
-			${spc} ${2}/live/${f} ${CONF_target}/live
+			${spc} ${2}/live/${f} ${4}/live
 		else
 			dqb "${1}/live/${f}"
-			${spc} ${1}/live/${f} ${CONF_target}/live
+			${spc} ${1}/live/${f} ${4}/live
 		fi
 		
 		dqb "NECKST"
@@ -54,77 +50,81 @@ function part0() {
 	done
 
 	#efi uutena 13725
-	dqb "${spc} -a ${1}/efi ${CONF_target}"
-	${spc} -a ${1}/efi ${CONF_target}
+	dqb "${spc} -a ${1}/efi ${4}"
+	${spc} -a ${1}/efi ${4}
 	csleep 1
 
-	#lähde voi olla muukin kuin mountattu .iso, siksi ei enää 	CONF_SOURCE
+	#lähde voi olla muukin kuin mountattu .iso, siksi ei enää CONF_SOURCE
 	bootloader ${3} ${2} ${1} 
 	
-	default_process ${CONF_target}/live
+	default_process ${4}/live
 	local src2=${2}/${TARGET_pad_dir}
+	${scm} o+w ${4}/${TARGET_pad_dir}
 
-	${scm} o+w ${CONF_target}/${TARGET_pad_dir}
-	${odio} touch ${CONF_target}/${TARGET_pad_dir}/${n}.conf
-	${scm} 0644 ${CONF_target}/${TARGET_pad_dir}/${n}.conf
-	${sco} ${n}:${n} ${CONF_target}/${TARGET_pad_dir}/${n}.conf
-	${scm} o-w ${CONF_target}/${TARGET_pad_dir}
+	fasdfasd ${4}/${TARGET_pad_dir}/${n}.conf
 
+	${scm} o-w ${4}/${TARGET_pad_dir}
 	dqb "BEFORE COPY_x"
 	csleep 1
 
 	[ -v TARGET_DIGESTS_dir ] || exit 666
 	[ -v TARGET_DGST0 ] || exit 666
-	dqb "OUYG)(&R()%¤ER"
+	dqb "OGDRU JAHA D 666"
 
 	[ -z ${TARGET_DIGESTS_dir} ] && exit 65
 	dqb "56448748765484"
 
 	[ -z ${TARGET_DGST0} ] && exit 66
-	dqb "ÄÖ_ÅPÄÖÖÅPO"
+	dqb "AAPPO.IPIIPPO"
 
 	#HUOM.11725:linkitys-syistä oli "/" 1. param lopussa, ehkä pois jatkossa ?
 
-	#TODO:$2/TARGET_pad kuitenkin? find voi perioaatteessa löytää väärää matskua...
-	copy_main ${2} ${CONF_target}/${TARGET_pad_dir} ${CONF_scripts_dir}
-	
-	#HUOM.141025:vaihdettu $2 -> $2/T_PAD koska Syyt
-	copy_conf ${2}/${TARGET_pad_dir} ${n} ${CONF_target}/${TARGET_pad_dir}
-
-	#HUOM.141025:vaihdettu $2 -> $2/T_PAD koska Syyt
-	copy_sums ${2}/${TARGET_DIGESTS_dir} ${CONF_target}/${TARGET_DIGESTS_dir}
+	copy_main ${2}/${TARGET_pad_dir} ${4}/${TARGET_pad_dir} ${CONF_scripts_dir}
+	copy_conf ${2}/${TARGET_pad_dir} ${n} ${4}/${TARGET_pad_dir}
+	copy_sums ${2}/${TARGET_DIGESTS_dir} ${4}/${TARGET_DIGESTS_dir}
 	
 	dqb "4FT3R COPY_X"
 	csleep 1
 
-	${odio} touch ${CONF_target}/${TARGET_pad_dir}/*
+	${odio} touch ${4}/${TARGET_pad_dir}/*
 	${scm} 0444 ${CONF_tmpdir}/*.conf
-	${scm} 0755 ${CONF_tmpdir}/*.sh
+	${scm} 0755 ${CONF_tmpdir}/*.sh #vaiko 0555?
 	
-	#TODO:sen pad-hmiston omistajuuden pakotus (d:d ei hyvä, ehkä)
-	#keys-hmistossa ei juuri nyt taida olla .gpg-tdstoja... (081025)
+	#161225:ao. tavalla jos mennäään ni parempi olla julk av erillisessä hmistossa q salaiset
+	#vähän parempi olisi jos voisi vielä rajata että mitkä .gpg tuolta hak alta kopsataan
 
-	${spc} ${CONF_keys_dir}/*.gpg ${CONF_target}/${TARGET_DIGESTS_dir}
-	default_process ${CONF_target}/${TARGET_pad_dir}
+	if [ -v CONF_keys_dir_pub ] ; then #keys.conf.. eiku common.conf ainakin vielä
+		if [ ! -z ${CONF_keys_dir_pub}} ] ; then
+			if [ -d ${CONF_keys_dir_pub} ] ; then
+				dqb "${spc} ${CONF_keys_dir_pub}/*.gpg ${4}/${TARGET_DIGESTS_dir}"
+				csleep 1
+				${spc} ${CONF_keys_dir_pub}/*.gpg ${4}/${TARGET_DIGESTS_dir}
+			fi
+		fi
+	fi
 
-	${scm} 0555 ${CONF_target}/${TARGET_pad_dir}/*.sh
-	${sco} -R ${n}:${n} ${CONF_target}/${TARGET_DIGESTS_dir}
+	default_process ${4}/${TARGET_pad_dir}
+	[ ${debug} -eq 1 ] && ls -las ${4}
+	csleep 10
+
+	${scm} 0555 ${4}/${TARGET_pad_dir}/*.sh
+	${sco} -R ${n}:${n} ${4}/${TARGET_DIGESTS_dir}
 	
-	${scm} 0555 ${CONF_target}/live
-	${scm} 0755 ${CONF_target}/${TARGET_DIGESTS_dir}
+	${scm} 0555 ${4}/live
+	${scm} 0755 ${4}/${TARGET_DIGESTS_dir}
 
 	dqb "part0 d0ne"
 }
 
-echo "src= ${1} , stc2= ${2} , bl= ${3}"
+dqb "src= ${1} , stc2= ${2} , bl= ${3}"
 [ -v CONF_source ] || exit 666
 [ -v CONF_target ] || exit 666
 make_tgt_dirs ${CONF_target} ${CONF_source} ${3}
 
 if [ -d ${1} ] ; then
-	part0 ${1} ${2} ${3}
+	part0 ${1} ${2} ${3} ${CONF_target}
 else
-	if [ -s ${1} ] && [ -r ${1} ] ; then #vieläkö tässä jokin qsee 111025?
+	if [ -s ${1} ] && [ -r ${1} ] ; then #151225:nyt toimii kun common_funcs muutettu
 		dqb "${som} -o loop,ro ${1} ${CONF_source}"
 		csleep 3
 
@@ -132,7 +132,7 @@ else
 		[ $? -eq 0 ] || exit 666
 		sleep 6
 
-		part0 ${CONF_source} ${2} ${3}	
+		part0 ${CONF_source} ${2} ${3} ${CONF_target}	
 		${uom} ${CONF_source} 
 	else
 		echo "https://www.youtube.com/watch?v=KnH2dxemO5o";exit 666	
