dd18be5de674bbe7c37dfef7258df5a627371457
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-12-16 17:46:55 +0000

    Add files via upload

--- skripts/imp2.sq-chroot.sh
+++ skripts/imp2.sq-chroot.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#tarpeellinen kikkare? jos on ni pirtäisi päivittää (16.12.25)
 ##echo "PT 1:extract some archives "
 ##echo "PT 2:make a conf-file before PT 1 "
 ##pitää se common_lib myös jos haluaa -1:sen asentavan asmalla pakettei
