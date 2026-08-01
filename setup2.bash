82f130bd9869bea3343511ce3384603b89aee14f
senescent777 <113385047+senescent777@users.noreply.github.com>
2026-08-01 19:12:07 +0300

    recent/010826

--- setup2.bash
+++ setup2.bash
@@ -45,10 +45,12 @@ smr="${odio} rm"
 #simppelimpi näin
 [ -v CONF_iface ] && ${odio} ip link set ${CONF_iface} down
 
-#ao. fktioissa olisi kai hyvä ol,la enemmän tarkistukisa?
+#ao. fktioissa olisi kai hyvä ol,la enemmän tarkistukisa? nyt on
 function reqwreqw() {
 	[ -z "${1}" ] && exit 99
 	[ -f ${1} ] || exit 100
+	[ "${1}" == "/" ] && exit 101
+
 	csleep 1
 	${sco} 0:0 ${1}
 	${scm} a-w ${1}
@@ -56,6 +58,7 @@ function reqwreqw() {
 
 function fasdfasd() {
 	[ -z "${1}" ] && exit 99
+	[ "${1}" == "/" ] && exit 101
 
 	csleep 1
 	${odio} touch ${1}
@@ -73,6 +76,7 @@ frist=0
 
 function ekf() {
 	dqb "EKF (${1})"
+	#[ -z "${1}" ] && exit 99
 	csleep 2
 	local t=$(${odio} which ${1})
 
@@ -95,8 +99,8 @@ g_aa=${CONF_aa}
 g_ab=${CONF_ab}
 
 function jord() {
-	[ -z "${1}" ] && exit 666
-	[ -d ${1} ] || exit 666
+	[ -z "${1}" ] && exit 66
+	[ -d ${1} ] || exit 65
 	
 	dqb "jord"
 	csleep 1
@@ -114,19 +118,19 @@ function jord() {
 #... vaikuttaisi että gdoit.sh kehitysymp saattaa paskoa slimin (v8elä 07/26?)
 
 function aqua() {
-	dqb "aqua"
+	dqb "aqua ) ${1} ("
 	csleep 1
 
-	[ -v CONF_pkgsrc ] || exit 22
-	[ -z "${CONF_pkgsrc}" ] && exit 21
-	[ -d ${CONF_pkgsrc} ] || exit 23
+	#[ -v CONF_pkgsrc ] || exit 22 
+	[ -z "${1}" ] && exit 21
+	[ -d ${1} ] || exit 23
 
 	${odio} apt --fix-broken install
-	dqb "VAIH:  coreutils,git,libdev,libjte,dmsetup AJAN TASALLE JOS EI OLE JO"
+	dqb "VAIH:  ,libdev,libjte,dmsetup AJAN TASALLE JOS EI OLE JO (exp2 s tjsp)"
 	csleep 6
 
 	local q=$(mktemp -d)
-	${spc} ${CONF_pkgsrc}/*.deb ${q}
+	${spc} ${1}/*.deb ${q}
 	[ $? -eq 0 ] || exit 4
 
 	#parempi samaan aikaan dms ja libdev
@@ -166,6 +170,10 @@ function aqua() {
 function ignis() {
 	dqb "ignis"
 	csleep 1
+
+	[ -z "${1}" ] && exit 21
+	[ -d ${1} ] || exit 23
+
 	local tig=$(${odio} which git)
 
 	[ -z "${tig}" ] && exit 68
@@ -176,8 +184,8 @@ function ignis() {
 	dqb "tg1,1,dibe"
 
 	#varmaan olisi hyvä testata tämä blokki josqs
-	if [ -s ${CONF_basedir}/.gitignore ] ; then
-		echo "not touching ${CONF_basedir}/.gitignore this time"
+	if [ -s ${1}/.gitignore ] ; then
+		echo "not touching ${1}/.gitignore this time"
 	else
 		echo "setup1 may have done this already?"
 	fi
@@ -186,12 +194,12 @@ function ignis() {
 	sleep 5
 	local c=0
 
-	if [ -d ${CONF_basedir}/.git ] ; then
-		c=$(find ${CONF_basedir}/.git -type f | wc -l)
+	if [ -d ${1}/.git ] ; then
+		c=$(find ${1}/.git -type f | wc -l)
 	fi
 
 	if [ ${c} -lt 1 ] ; then
-		echo "SHOULD ${tig} init ${CONF_basedir}"
+		echo "SHOULD ${tig} init ${1}"
 	fi
 }
 
@@ -249,44 +257,49 @@ function f5a() {
 	dqb "F5.a"
 	csleep 5
 
-
-	#VAIH:param tarq? /tmp löytymminen vielä
 	[ -z "${1}" ] && exit 99
-	[ -z "${2}" ] && exit 99
+	[ -z "${2}" ] && exit 98
+
+	#010826;tokan param kanssa ok grepata tnp
+	local x=$(echo ${2} | grep tmp | wc -l)
+	[ ${x} gt 0 ] || exit 95 
+
+	[ -z "${3}" ] && exit 97
+	[ -d "${3}" ] || exit 96
 
 	fasdfasd ${1} #kuinka tarpeellinen param?
 	fasdfasd ${2}
 
 	#CB_LIST1="$(${odio} which halt) $(${odio} which reboot) /usr/bin/which ${sifu} ${sifd}"
 	#...ao lista mukaan aa:han vaiko common_lib kanssa jogtain jatkosöäätöä?
-	[ -v CONF_scripts_dir ] || exit 11
-	[ -z "${CONF_scripts_dir}" ] && exit 22
-	[ -d ${CONF_scripts_dir} ] || exit 33
+	[ -v CONF_scripts_dir ] || exit 11 #kutsuvaan koodiin
+
 
 	#muistettava kanssa varmistaa että dalek tulee kaikkiin sitä tarvitseviin juttuihin mukaan?
 
 	dqb "MAKING OF:dalek.bash"
-	[ -f ${CONF_scripts_dir}/dalek.bash ] && ${svm} ${CONF_scripts_dir}/dalek.bash ${CONF_scripts_dir}/dalek.bash.OLD
+	[ -f ${3}/dalek.bash ] && ${svm} ${1}/dalek.bash ${1}/dalek.bash.OLD
 	csleep 3
 
 	#15726:jos on pedantti niin dalek.s validius pitäisi tarkistaa ennenq lisäilee sudoersiin juttuja
-	head -n 1 ${CONF_scripts_dir}/dalek.s > ${2}
-	grep -v "#" ${CONF_scripts_dir}/common.conf >> ${2}
-	grep -v "#" ${CONF_scripts_dir}/dalek.s >> ${2}
+	head -n 1 ${3}/dalek.s > ${2}
+	grep -v "#" ${3}/common.conf >> ${2}
+	grep -v "#" ${3}/dalek.s >> ${2}
 
-	reqwreqw ${CONF_scripts_dir}/dalek.s #jos voisi olla renkkaamatta vähän aikaa
+	reqwreqw ${3}/dalek.s #jos voisi olla renkkaamatta vähän aikaa
 	reqwreqw ${2}
-	${svm} ${2} ${CONF_scripts_dir}/dalek.bash
+	${svm} ${2} ${3}/dalek.bash
 
-	${scm} a+x ${CONF_scripts_dir}/dalek.bash
-	ls -las ${CONF_scripts_dir}/dalek.*
+	${scm} a+x ${3}/dalek.bash
+	ls -las ${3}/dalek.*
 	
 	csleep 3
 	dqb "AFTER DALEK"
 	csleep 3
 	
 	#VAIH:pitäisi saada aikaiseksi testata erinäiset skriptit omegan ajon jälkeen, sitä ennen jos toimii niin ei kerro juuri mitään
-	#... josko 07/26 aikana valmiiksi?
+	#... josko 08/26 aikana valmiiksi?
+	#btw. miksi erikseen esab? , olisi suoraan tuo $3/dalek ... polut?
 
 	local t=$(${odio} find ${CONF_esab} -type f -name "dalek.bash")
 	echo "t= ${t}"
@@ -302,7 +315,7 @@ function f5b() {
 	local c2
 	local t
 
-	#VAIH:param tarq? /tmp löytymminen vielä
+	#mitenkähän tämän tark pitäisi mennä?
 	[ -z "${1}" ] && exit 99
 
 	#... toisaalta squashfs-työkaluja ei tarvitsisi sudottaa (?)
@@ -318,7 +331,7 @@ function f5b() {
 
 	#g_ab juttuja lukuunottamatta asiat jo kuynnossa?
 	#15726: $1 kanssa jokin tr-jekku jatkossa? kts "man 5 sudoers"
-	#jekku jo tehty?
+	#jekku jo tehty? bissiin
 
 	t=$(echo ${1} | tr -dc a-zA-Z0-9/_-)
 	[ -z "${t}" ] && exit 99
@@ -348,11 +361,11 @@ jord ${CONF_basedir}
 
 #se "komentorivi-vipu millä pelkstään sorkitaan dalek ja sudoers"
 if [ "${1}" != "1" ] ; then
-	[ -s ${CONF_scripts_dir}/dalek.bash ] || aqua
+	[ -s ${CONF_scripts_dir}/dalek.bash ] || aqua ${CONF_pkgsrc}
 	[ -v CONF_ue ] || exit 34
 	[ -v CONF_un ] || exit 35
 
-	ignis #${CONF_basedir}
+	ignis ${CONF_basedir}
 	[ -v CONF_dir ] || exit 44
 	[ -d ${CONF_dir} ] || exit 45
 
@@ -363,7 +376,7 @@ somefile=$(mktemp qsipasq2-XXXX )
 #virhetilanteeseen reagointi mktemp kanssa?
 somefile2=$(mktemp) #ehkä pärjäisi ilmankin tuon kanssa kikkailua, suoraan kohde-hmistooon tdsto ja täts it
 
-f5a ${somefile} ${somefile2} 
+f5a ${somefile} ${somefile2} ${CONF_scripts_dir}
 f5b ${somefile}
 
 echo "kutl v | g_doit -v 1 ?"
